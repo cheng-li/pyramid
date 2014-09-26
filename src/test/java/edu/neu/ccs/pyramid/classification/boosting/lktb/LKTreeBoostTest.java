7997845330cf9eca33535fd9ac2570a2477c0d59
chengli <chengli.email@gmail.com>
2014-09-26 15:26:57 -0400

    git test 2

--- src/test/java/edu/neu/ccs/pyramid/classification/boosting/lktb/LKTreeBoostTest.java
+++ src/test/java/edu/neu/ccs/pyramid/classification/boosting/lktb/LKTreeBoostTest.java
@@ -11,6 +11,7 @@ import java.util.concurrent.Executors;
 
 public class LKTreeBoostTest {
     public static void main(String[] args) throws Exception {
+        System.out.println("");
         spam_test();
 //        newsgroup_test();
 //        spam_build();
