717265bbda2026af5d42cfdf9a60491e02cdd606
Rainicy <rainicy925@gmail.com>
2014-09-30 20:03:40 -0400

    Add Histogram Naive Bayes

---
+++ src/main/java/edu/neu/ccs/pyramid/classification/naive_bayes/HistogramNaiveBayes.java
@@ -0,0 +1,25 @@
+package edu.neu.ccs.pyramid.classification.naive_bayes;
+
+import edu.neu.ccs.pyramid.classification.Classifier;
+import edu.neu.ccs.pyramid.classification.ProbabilityEstimator;
+import edu.neu.ccs.pyramid.dataset.FeatureRow;
+
+/**
+ * Created by Rainicy on 9/30/14.
+ */
+public class HistogramNaiveBayes implements Classifier, ProbabilityEstimator {
+    @Override
+    public int predict(FeatureRow featureRow) {
+        return 0;
+    }
+
+    @Override
+    public int getNumClasses() {
+        return 0;
+    }
+
+    @Override
+    public double[] predictClassProbs(FeatureRow featureRow) {
+        return new double[0];
+    }
+}
