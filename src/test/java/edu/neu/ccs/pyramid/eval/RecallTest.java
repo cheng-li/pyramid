539501274e0fe808309348498e598ab34eceef65
chengli <chengli.email@gmail.com>
2014-10-02 23:55:10 -0400

    add class Recall

---
+++ src/test/java/edu/neu/ccs/pyramid/eval/RecallTest.java
@@ -0,0 +1,16 @@
+package edu.neu.ccs.pyramid.eval;
+
+import static org.junit.Assert.*;
+
+public class RecallTest {
+    public static void main(String[] args) {
+        test1();
+    }
+
+    private static void test1(){
+        int[] predictions = {0,0,1,1,1};
+        int[] labels = {0,0,0,1,1};
+        System.out.println(Recall.recall(labels,predictions,0));
+    }
+
+}
