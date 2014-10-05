e18bfbc19f0357ded246b5d10409a8c2274e1505
chengli <chengli.email@gmail.com>
2014-10-04 22:41:11 -0400

    add class LKTBFactory

---
+++ src/main/java/edu/neu/ccs/pyramid/classification/boosting/lktb/LKTBFactory.java
@@ -0,0 +1,50 @@
+package edu.neu.ccs.pyramid.classification.boosting.lktb;
+
+import edu.neu.ccs.pyramid.classification.Classifier;
+import edu.neu.ccs.pyramid.classification.ClassifierFactory;
+import edu.neu.ccs.pyramid.classification.TrainConfig;
+import edu.neu.ccs.pyramid.dataset.ClfDataSet;
+
+import java.io.*;
+
+/**
+ * Created by chengli on 10/4/14.
+ */
+public class LKTBFactory implements ClassifierFactory{
+
+    @Override
+    public Classifier train(ClfDataSet dataSet, TrainConfig config) {
+        if (!(config instanceof LKTBTrainConfig)){
+            throw new IllegalArgumentException("!(config instanceof LKTBTrainConfig)");
+        }
+        LKTBTrainConfig lktbTrainConfig = (LKTBTrainConfig)config;
+        return train(dataSet,lktbTrainConfig);
+    }
+
+    public LKTreeBoost train(ClfDataSet dataSet, LKTBTrainConfig config){
+        LKTBConfig lktbConfig = new LKTBConfig.Builder(dataSet)
+                .numLeaves(config.getNumLeaves())
+                .learningRate(config.getLearningRate())
+                .minDataPerLeaf(config.getMinDataPerLeaf())
+                .dataSamplingRate(config.getDataSamplingRate())
+                .featureSamplingRate(config.getFeatureSamplingRate())
+                .numSplitIntervals(config.getNumSplitIntervals())
+                .build();
+        LKTreeBoost lkTreeBoost = new LKTreeBoost(dataSet.getNumClasses());
+        if (config.usePrior()){
+            lkTreeBoost.setPriorProbs(dataSet);
+        }
+        lkTreeBoost.setTrainConfig(lktbConfig);
+        for (int iteration=0;iteration<config.getNumIterations();iteration++){
+            lkTreeBoost.boostOneRound();
+        }
+        return lkTreeBoost;
+    }
+
+
+
+    @Override
+    public Classifier deserialize(File file) throws Exception {
+        return LKTreeBoost.deserialize(file);
+    }
+}
---
+++ src/test/java/edu/neu/ccs/pyramid/classification/boosting/lktb/LKTBFactoryTest.java
@@ -0,0 +1,65 @@
+package edu.neu.ccs.pyramid.classification.boosting.lktb;
+
+import edu.neu.ccs.pyramid.classification.Classifier;
+import edu.neu.ccs.pyramid.classification.TrainConfig;
+import edu.neu.ccs.pyramid.configuration.Config;
+import edu.neu.ccs.pyramid.dataset.ClfDataSet;
+import edu.neu.ccs.pyramid.dataset.DataSetType;
+import edu.neu.ccs.pyramid.dataset.TRECFormat;
+import edu.neu.ccs.pyramid.eval.Accuracy;
+import edu.neu.ccs.pyramid.eval.ConfusionMatrix;
+import edu.neu.ccs.pyramid.eval.PerClassMeasures;
+import org.apache.commons.lang3.time.StopWatch;
+
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+public class LKTBFactoryTest {
+    private static final Config config = new Config("configs/local.config");
+    private static final String DATASETS = config.getString("input.datasets");
+    private static final String TMP = config.getString("output.tmp");
+    public static void main(String[] args) throws Exception{
+        spam_build();
+        spam_load();
+
+    }
+    static void spam_load() throws Exception{
+        System.out.println("loading classifier");
+        Classifier classifier = new LKTBFactory().deserialize(new File(TMP,"classifier.ser"));
+        ClfDataSet dataSet = TRECFormat.loadClfDataSet(new File(DATASETS, "/spam/trec_data/test.trec"),
+                DataSetType.CLF_DENSE, true);
+        System.out.println("test data:");
+        System.out.println(dataSet.getMetaInfo());
+
+
+        double accuracy = Accuracy.accuracy(classifier, dataSet);
+        System.out.println(accuracy);
+        ConfusionMatrix confusionMatrix = new ConfusionMatrix(2,classifier,dataSet);
+        System.out.println("confusion matrix:");
+        System.out.println(confusionMatrix.printWithExtLabels());
+
+
+    }
+
+    static void spam_build() throws Exception{
+
+
+        ClfDataSet dataSet = TRECFormat.loadClfDataSet(new File(DATASETS,"/spam/trec_data/train.trec"),
+                DataSetType.CLF_DENSE,true);
+        System.out.println(dataSet.getMetaInfo());
+
+        TrainConfig trainConfig = new LKTBTrainConfig()
+                .setLearningRate(0.1)
+                .setNumIterations(1000)
+                .setNumLeaves(7);
+        Classifier classifier = new LKTBFactory().train(dataSet,trainConfig);
+
+
+        double accuracy = Accuracy.accuracy(classifier,dataSet);
+        System.out.println(accuracy);
+
+        classifier.serialize(new File(TMP,"classifier.ser"));
+    }
+
+}
