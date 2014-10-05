747cd3642d3cdcaced3ec5fe50265c3a21a50f33
chengli <chengli.email@gmail.com>
2014-10-05 00:01:44 -0400

    ClassifierFactory extends Serializable

--- src/main/java/edu/neu/ccs/pyramid/classification/ClassifierFactory.java
+++ src/main/java/edu/neu/ccs/pyramid/classification/ClassifierFactory.java
@@ -4,11 +4,12 @@ import edu.neu.ccs.pyramid.dataset.ClfDataSet;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Serializable;
 
 /**
  * Created by chengli on 10/4/14.
  */
-public interface ClassifierFactory {
+public interface ClassifierFactory extends Serializable{
     Classifier train(ClfDataSet dataSet, TrainConfig config);
     Classifier deserialize(File file) throws Exception;
     default Classifier deserialize(String file) throws Exception{
