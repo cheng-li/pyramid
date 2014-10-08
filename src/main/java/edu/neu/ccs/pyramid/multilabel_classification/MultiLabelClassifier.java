1159ab4e8d376c3fc717f392e11a7290a22989aa
chengli <chengli.email@gmail.com>
2014-10-08 15:46:14 -0400

    MultiLabelClassifier extends Serializable

--- src/main/java/edu/neu/ccs/pyramid/multilabel_classification/MultiLabelClassifier.java
+++ src/main/java/edu/neu/ccs/pyramid/multilabel_classification/MultiLabelClassifier.java
@@ -4,6 +4,7 @@ import edu.neu.ccs.pyramid.dataset.FeatureRow;
 import edu.neu.ccs.pyramid.dataset.MultiLabel;
 import edu.neu.ccs.pyramid.dataset.MultiLabelClfDataSet;
 
+import java.io.*;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -11,12 +12,30 @@ import java.util.stream.IntStream;
 /**
  * Created by chengli on 9/27/14.
  */
-public interface MultiLabelClassifier {
+public interface MultiLabelClassifier extends Serializable{
     MultiLabel predict(FeatureRow featureRow);
     default List<MultiLabel> predict(MultiLabelClfDataSet dataSet){
         return IntStream.range(0,dataSet.getNumDataPoints()).parallel()
                 .mapToObj(i -> predict(dataSet.getFeatureRow(i)))
                 .collect(Collectors.toList());
+    }
+
+    default void serialize(File file) throws Exception{
+        File parent = file.getParentFile();
+        if (!parent.exists()){
+            parent.mkdirs();
+        }
+        try (
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+                BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(bufferedOutputStream);
+        ){
+            objectOutputStream.writeObject(this);
+        }
+    }
+
+    default void serialize(String file) throws Exception{
+        serialize(new File(file));
     }
 
 }
