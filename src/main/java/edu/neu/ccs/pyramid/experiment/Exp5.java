43b5ba43bd292c42cebe46aac3d31f37ec4e66cc
chengli <chengli.email@gmail.com>
2014-10-09 22:01:58 -0400

    use a config file

--- src/main/java/edu/neu/ccs/pyramid/experiment/Exp5.java
+++ src/main/java/edu/neu/ccs/pyramid/experiment/Exp5.java
@@ -1,5 +1,6 @@
 package edu.neu.ccs.pyramid.experiment;
 
+import edu.neu.ccs.pyramid.configuration.Config;
 import edu.neu.ccs.pyramid.data_formatter.cnn.IndexBuilder;
 import edu.neu.ccs.pyramid.elasticsearch.ESIndex;
 import edu.neu.ccs.pyramid.elasticsearch.ESIndexBuilder;
@@ -16,14 +17,20 @@ import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 
 
 /**
- * build cnn index on laptop
+ * build cnn index
  * Created by chengli on 9/13/14.
  */
 public class Exp5 {
     public static void main(String[] args) throws Exception{
-        String file = "/Users/chengli/Datasets/cnn/articles";
+        if (args.length !=1){
+            throw new IllegalArgumentException("please specify the config file");
+        }
 
-        Node node = nodeBuilder().client(true).clusterName("elasticsearch").node();
+        Config config = new Config(args[0]);
+        System.out.println(config);
+        String file = config.getString("input.file");
+
+        Node node = nodeBuilder().client(true).clusterName(config.getString("index.clusterName")).node();
         Client client = node.client();
         try(BufferedReader br = new BufferedReader(new FileReader(new File(file)))){
             String line = null;
