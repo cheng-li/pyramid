30900ac38c72afebed72155ee60dab03508eff13
chengli <chengli.email@gmail.com>
2014-10-13 12:47:49 -0400

    add default method addLabels

--- src/main/java/edu/neu/ccs/pyramid/dataset/MultiLabelClfDataSet.java
+++ src/main/java/edu/neu/ccs/pyramid/dataset/MultiLabelClfDataSet.java
@@ -1,10 +1,19 @@
 package edu.neu.ccs.pyramid.dataset;
 
+import java.util.Collection;
+
 /**
  * Created by chengli on 9/27/14.
  */
 public interface MultiLabelClfDataSet extends DataSet{
     MultiLabel[] getMultiLabels();
     void addLabel(int dataPointIndex, int classIndex);
+
+    default void addLabels(int dataPointIndex, Collection<Integer> labels){
+        for (Integer label: labels){
+            addLabel(dataPointIndex,label);
+        }
+    }
+
     int getNumClasses();
 }
