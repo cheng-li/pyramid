39f2f17cee2cff0df21ef9cc050b8854e7bb9c1c
chengli <chengli.email@gmail.com>
2014-10-21 21:07:51 -0400

    add Formatter for mnist

---
+++ src/main/java/edu/neu/ccs/pyramid/data_formatter/mnist/Formatter.java
@@ -0,0 +1,50 @@
+package edu.neu.ccs.pyramid.data_formatter.mnist;
+
+import edu.neu.ccs.pyramid.configuration.Config;
+import edu.neu.ccs.pyramid.dataset.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by chengli on 10/21/14.
+ */
+public class Formatter {
+    private static final Config config = new Config("configs/local.config");
+    private static final String DATASETS = config.getString("input.datasets");
+    private static final String TMP = config.getString("output.tmp");
+    public static void main(String[] args) throws Exception{
+        dumpTrain();
+        dumpTest();
+
+    }
+
+    private static void dumpTrain() throws Exception{
+        File featureFile = new File(DATASETS,"mnist/training_image.txt");
+        File labelFile = new File(DATASETS,"mnist/training_label.txt");
+        ClfDataSet dataSet = StandardFormat.loadClfDataSet(10, featureFile, labelFile, ",", DataSetType.CLF_DENSE);
+        List<String> extLabels = new ArrayList<>();
+        for (int i=0;i<10;i++){
+            extLabels.add(""+i);
+        }
+
+        LabelTranslator labelTranslator = new LabelTranslator(extLabels);
+        DataSetUtil.setLabelTranslator(dataSet,labelTranslator);
+        TRECFormat.save(dataSet, new File(TMP, "train.trec"));
+    }
+
+    private static void dumpTest() throws Exception{
+        File featureFile = new File(DATASETS,"mnist/testing_image.txt");
+        File labelFile = new File(DATASETS,"mnist/testing_label.txt");
+        ClfDataSet dataSet = StandardFormat.loadClfDataSet(10, featureFile, labelFile, ",", DataSetType.CLF_DENSE);
+        List<String> extLabels = new ArrayList<>();
+        for (int i=0;i<10;i++){
+            extLabels.add(""+i);
+        }
+
+        LabelTranslator labelTranslator = new LabelTranslator(extLabels);
+        DataSetUtil.setLabelTranslator(dataSet,labelTranslator);
+        TRECFormat.save(dataSet, new File(TMP, "test.trec"));
+    }
+}
