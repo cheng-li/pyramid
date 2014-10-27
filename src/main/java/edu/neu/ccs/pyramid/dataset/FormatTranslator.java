ae8ac05a9da5c91df228b0855a2c61b4517c13ff
chengli <chengli.email@gmail.com>
2014-10-27 16:30:51 -0400

    add more methods

--- src/main/java/edu/neu/ccs/pyramid/dataset/FormatTranslator.java
+++ src/main/java/edu/neu/ccs/pyramid/dataset/FormatTranslator.java
@@ -6,7 +6,11 @@ import java.io.File;
  * Created by chengli on 10/27/14.
  */
 public class FormatTranslator {
-    public static void trecToLIBSVM(File trecFile, File libSVMFile){
+    public static void trecToLibSvm(File trecFile, File libSvmFile){
+
+    }
+
+    public static void trecToWeka(File trecFile, File wekaFile){
         
     }
 }
