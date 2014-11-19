5e3670c56d8030e12628539b1b19d1555c94c54c
chengli <chengli.email@gmail.com>
2014-11-18 22:04:15 -0500

    bug fix

--- src/main/java/edu/neu/ccs/pyramid/dataset/MLTKFormat.java
+++ src/main/java/edu/neu/ccs/pyramid/dataset/MLTKFormat.java
@@ -1,9 +1,6 @@
 package edu.neu.ccs.pyramid.dataset;
 
-import mltk.core.Attribute;
-import mltk.core.Instance;
-import mltk.core.Instances;
-import mltk.core.NumericalAttribute;
+import mltk.core.*;
 import org.apache.mahout.math.Vector;
 
 import java.util.ArrayList;
@@ -17,16 +14,23 @@ public class MLTKFormat {
         List<Attribute> attributes = new ArrayList<>();
         for (int j=0;j<dataSet.getNumFeatures();j++){
             String name = dataSet.getFeatureSetting(j).getFeatureName();
-            attributes.add(new NumericalAttribute(name));
+            attributes.add(new NumericalAttribute(name,j));
         }
-        Instances instances = new Instances(attributes);
+        String[] states = new String[dataSet.getNumClasses()];
+        for (int k=0;k<states.length;k++){
+            states[k]=""+k;
+        }
+
+        Attribute targetAttribute = new NominalAttribute("target",states);
+
+        Instances instances = new Instances(attributes, targetAttribute);
         for (int i=0;i<dataSet.getNumDataPoints();i++){
             instances.add(toInstance(dataSet,i));
         }
         return instances;
     }
-    
-    public static Instance toInstance(ClfDataSet dataSet, int dataPointIndex){
+
+    static Instance toInstance(ClfDataSet dataSet, int dataPointIndex){
         double[] features = new double[dataSet.getNumFeatures()];
         Vector vector = dataSet.getRow(dataPointIndex);
         for (Vector.Element element: vector.nonZeroes()){
---
+++ src/test/java/edu/neu/ccs/pyramid/dataset/MLTKFormatTest.java
@@ -0,0 +1,72 @@
+package edu.neu.ccs.pyramid.dataset;
+
+import edu.neu.ccs.pyramid.configuration.Config;
+import edu.neu.ccs.pyramid.eval.Accuracy;
+import mltk.core.Instances;
+import mltk.core.io.InstancesReader;
+import mltk.predictor.Learner;
+import mltk.predictor.glm.GLM;
+import mltk.predictor.glm.LassoLearner;
+
+import java.io.File;
+import java.util.stream.IntStream;
+
+import static org.junit.Assert.*;
+
+public class MLTKFormatTest {
+    private static final Config config = new Config("configs/local.config");
+    private static final String DATASETS = config.getString("input.datasets");
+    private static final String TMP = config.getString("output.tmp");
+
+    public static void main(String[] args) throws Exception{
+        test1();
+    }
+
+    private static void test1() throws Exception{
+        GLM glm = spamTrain();
+        spamTest(glm);
+    }
+
+    private static GLM spamTrain() throws Exception{
+        ClfDataSet dataSet = TRECFormat.loadClfDataSet(new File(DATASETS,"/spam/trec_data/train.trec"),
+                DataSetType.CLF_DENSE,true);
+        System.out.println(dataSet.getRow(0));
+        Instances trainSet = MLTKFormat.toInstances(dataSet);
+        System.out.println(trainSet.get(0));
+        System.out.println(trainSet.dimension());
+        System.out.println(trainSet.getAttributes());
+        System.out.println(trainSet.getTargetAttribute());
+        LassoLearner learner = new LassoLearner();
+        learner.setTask(Learner.Task.CLASSIFICATION);
+        learner.setMaxNumIters(100);
+        learner.setVerbose(false);
+        learner.setLambda(0.1);
+        GLM glm = learner.build(trainSet);
+        int[] predictions= IntStream.range(0, trainSet.size()).map(i-> glm.classify(trainSet.get(i)))
+                .toArray();
+        int[] labels = IntStream.range(0,trainSet.size()).map(i-> (int)(trainSet.get(i).getTarget()))
+                .toArray();
+        System.out.println("accuracy on the training set = "+ Accuracy.accuracy(labels, predictions));
+        return glm;
+    }
+
+    private static void spamTest(GLM glm) throws Exception{
+        ClfDataSet dataSet = TRECFormat.loadClfDataSet(new File(DATASETS, "/spam/trec_data/test.trec"),
+                DataSetType.CLF_DENSE, true);
+        Instances testSet = MLTKFormat.toInstances(dataSet);
+        int[] predictions= IntStream.range(0,testSet.size()).map(i-> glm.classify(testSet.get(i)))
+                .toArray();
+        int[] labels = IntStream.range(0,testSet.size()).map(i-> (int)(testSet.get(i).getTarget()))
+                .toArray();
+        System.out.println("accuracy on the test set = "+ Accuracy.accuracy(labels,predictions));
+    }
+
+    private static void test2() throws Exception{
+        String matrixFile = new File(new File(DATASETS,"/spam/trec_data/train.trec"), "feature_matrix.txt").getAbsolutePath();
+        Instances trainSet = InstancesReader.read(null, matrixFile);
+        System.out.println(trainSet.getAttributes());
+        System.out.println(trainSet.getTargetAttribute());
+
+    }
+
+}
