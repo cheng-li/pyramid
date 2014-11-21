54ca48cb4f7b385ecb790e2c9cac8106c19bfbd0
chengli <chengli.email@gmail.com>
2014-11-21 12:02:45 -0500

    add Exp27

--- src/main/java/edu/neu/ccs/pyramid/experiment/ExpIndex.java
+++ src/main/java/edu/neu/ccs/pyramid/experiment/ExpIndex.java
@@ -28,6 +28,7 @@ package edu.neu.ccs.pyramid.experiment;
  * 24: index imdb
  * 25: elastic net, single label
  * 26: gradient boosting hyper parameter tuning, single label
+ * 27: lasso hyper parameter tuning
  */
 public class ExpIndex {
 
