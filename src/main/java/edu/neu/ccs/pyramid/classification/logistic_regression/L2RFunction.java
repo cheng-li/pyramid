e21e4ab62d50305264642cc1a03a690a9402f2c0
chengli <chengli.email@gmail.com>
2014-11-28 12:57:31 -0500

    preprocess data for logistic regression

--- src/main/java/edu/neu/ccs/pyramid/classification/logistic_regression/L2RFunction.java
+++ src/main/java/edu/neu/ccs/pyramid/classification/logistic_regression/L2RFunction.java
@@ -1,6 +1,8 @@
 package edu.neu.ccs.pyramid.classification.logistic_regression;
 
 import edu.neu.ccs.pyramid.dataset.ClfDataSet;
+import edu.neu.ccs.pyramid.dataset.DataSet;
+import edu.neu.ccs.pyramid.dataset.DataSetBuilder;
 import org.apache.mahout.math.DenseVector;
 import org.apache.mahout.math.Vector;
 
@@ -25,19 +27,25 @@ public class L2RFunction {
      * the first column should be all 1
      * the labels should be 1/-1
      */
-    private final ClfDataSet dataSet;
+    private final DataSet dataSet;
+    private int[] labels;
+
+
+
+
 
     /**
      *
-     * @param dataSet
+     * @param clfDataSet
      * @param C constant vector
      */
-    public L2RFunction( ClfDataSet dataSet, Vector C ) {
-        int l = dataSet.getNumDataPoints();
-        this.dataSet = dataSet;
+    public L2RFunction( ClfDataSet clfDataSet, Vector C ) {
+        int l = clfDataSet.getNumDataPoints();
+        this.dataSet = addConstantColumn(clfDataSet);
         z = new DenseVector(l);
         D = new DenseVector(l);
         this.C = C;
+        this.labels = changeLabels(clfDataSet);
     }
 
 
@@ -56,7 +64,7 @@ public class L2RFunction {
 
     public double fun(Vector w) {
         double f = 0;
-        int[] y = dataSet.getLabels();
+        int[] y = labels;
         int l = dataSet.getNumDataPoints();
         Xv(w, z);
         f += w.dot(w);
@@ -74,7 +82,7 @@ public class L2RFunction {
 
     public void grad(Vector w, Vector g) {
 
-        int[] y = dataSet.getLabels();
+        int[] y = labels;
         int l = dataSet.getNumDataPoints();
         for (int i = 0; i < l; i++) {
             z.set(i, 1 / (1 + Math.exp(-y[i] * z.get(i))));
@@ -107,5 +115,51 @@ public class L2RFunction {
 
     public int get_nr_variable() {
         return dataSet.getNumFeatures();
+    }
+
+    public static DataSet addConstantColumn(ClfDataSet clfDataSet){
+        if (clfDataSet.hasMissingValue()){
+            throw new RuntimeException("cannot handle missing values in logistic regression");
+        }
+
+        DataSet dataSet1 = DataSetBuilder.getBuilder()
+                .numDataPoints(clfDataSet.getNumDataPoints())
+                .numFeatures(clfDataSet.getNumFeatures() + 1)
+                .dense(clfDataSet.isDense())
+                .missingValue(false)
+                .build();
+        for (int i=0;i<dataSet1.getNumDataPoints();i++){
+            //add constant 1
+            dataSet1.setFeatureValue(i,0,1);
+            //only copy non-zero elements
+            Vector vector = clfDataSet.getRow(i);
+            for (Vector.Element element: vector.nonZeroes()){
+                int featureIndex = element.index();
+                double value = element.get();
+                dataSet1.setFeatureValue(i,featureIndex+1,value);
+            }
+        }
+        return dataSet1;
+    }
+
+    /**
+     * change labels to 1/-1
+     * @param clfDataSet
+     * @return
+     */
+    public static int[] changeLabels(ClfDataSet clfDataSet){
+        if (clfDataSet.getNumClasses()!=2){
+            throw new RuntimeException("clfDataSet.getNumClasses()!=2");
+        }
+        int[] labels = clfDataSet.getLabels();
+        int[] changed = new int[labels.length];
+        for (int i=0;i<labels.length;i++){
+            if (labels[i]==0){
+                changed[i] = -1;
+            } else {
+                changed[i] = 1;
+            }
+        }
+        return changed;
     }
 }
