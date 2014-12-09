02e5124552527f73995929e4b9b1655b08768a0c
chengli <chengli.email@gmail.com>
2014-12-09 15:51:07 -0500

    rename variables

--- src/main/java/edu/neu/ccs/pyramid/optimization/GradientDescent.java
+++ src/main/java/edu/neu/ccs/pyramid/optimization/GradientDescent.java
@@ -6,12 +6,15 @@ import org.apache.mahout.math.Vector;
  * Created by chengli on 12/7/14.
  */
 public class GradientDescent {
-    private Optimizable.ByGradient function;
-    double learningRate;
+    private Optimizable.ByGradientValue function;
+    private BackTrackingLineSearcher lineSearcher;
 
-    public GradientDescent(Optimizable.ByGradient function, double learningRate) {
+
+    public GradientDescent(Optimizable.ByGradientValue function,
+                           double initialStepLength) {
         this.function = function;
-        this.learningRate = learningRate;
+        this.lineSearcher = new BackTrackingLineSearcher(function);
+        lineSearcher.setInitialStepLength(initialStepLength);
     }
 
     public void optimize(int numIterations){
@@ -23,9 +26,11 @@ public class GradientDescent {
     public void update(){
         Vector parameters = function.getParameters();
         Vector gradient = function.getGradient();
-        Vector updatedParams = parameters.minus(gradient.times(learningRate));
+        Vector direction = gradient.times(-1);
+        double stepLength = lineSearcher.findStepLength(direction);
+        System.out.println("stepLength="+stepLength);
+        Vector updatedParams = parameters.plus(direction.times(stepLength));
         parameters.assign(updatedParams);
         function.refresh();
-
     }
 }
