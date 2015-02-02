c57f84770fbc803de6e4a23efcd35f8a75569fc7
chengli <chengli.email@gmail.com>
2015-02-02 04:32:43 -0500

    add alternative implementations

--- src/main/java/edu/neu/ccs/pyramid/regression/regression_tree/Splitter.java
+++ src/main/java/edu/neu/ccs/pyramid/regression/regression_tree/Splitter.java
@@ -1,14 +1,13 @@
 package edu.neu.ccs.pyramid.regression.regression_tree;
 
+import com.google.common.util.concurrent.MoreExecutors;
 import edu.neu.ccs.pyramid.dataset.DataSet;
 import edu.neu.ccs.pyramid.dataset.FeatureType;
 import org.apache.commons.lang3.time.StopWatch;
 
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -16,6 +15,10 @@ import java.util.stream.IntStream;
  * Created by chengli on 8/6/14.
  */
 public class Splitter {
+//    static  ExecutorService executor = MoreExecutors.getExitingExecutorService((ThreadPoolExecutor) Executors
+//            .newFixedThreadPool(Runtime.getRuntime().availableProcessors()));
+
+
     /**
      *
      * @param regTreeConfig
@@ -28,7 +31,7 @@ public class Splitter {
                                        double[] probs){
         GlobalStats globalStats = new GlobalStats(labels,probs);
         int[] activeFeatures = regTreeConfig.getActiveFeatures();
-        Optional<SplitResult> result =  Arrays.stream(activeFeatures).parallel()
+        Optional<SplitResult> result = Arrays.stream(activeFeatures).parallel()
                 .mapToObj(featureIndex -> split(regTreeConfig,dataSet,labels,
                         probs,featureIndex,globalStats))
                 .filter(Optional::isPresent)
@@ -36,6 +39,56 @@ public class Splitter {
                 .max(Comparator.comparing(SplitResult::getReduction));
         return result;
     }
+
+
+//    /**
+//     *
+//     * @param regTreeConfig
+//     * @param probs
+//     * @return best valid splitResult, possibly nothing
+//     */
+//    static Optional<SplitResult> split(RegTreeConfig regTreeConfig,
+//                                       DataSet dataSet,
+//                                       double[] labels,
+//                                       double[] probs) {
+//
+//
+//        GlobalStats globalStats = new GlobalStats(labels,probs);
+//        int[] activeFeatures = regTreeConfig.getActiveFeatures();
+//
+//        List<Callable<Optional<SplitResult>>> tasks = new ArrayList<>(activeFeatures.length);
+//
+//        for (int i=0;i<activeFeatures.length;i++){
+//            int featureIndex = activeFeatures[i];
+//            tasks.add(()->split(regTreeConfig,dataSet,labels,probs,featureIndex,globalStats));
+//        }
+//
+//        List<Future<Optional<SplitResult>>> futures = null;
+//        try {
+//            futures = executor.invokeAll(tasks);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        List<Optional<SplitResult>> optionals = new ArrayList<>(futures.size());
+//        for (Future<Optional<SplitResult>> future: futures){
+//            Optional<SplitResult> optional = null;
+//            try {
+//                optional = future.get();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            } catch (ExecutionException e) {
+//                e.printStackTrace();
+//            }
+//            optionals.add(optional);
+//        }
+//
+//        Optional<SplitResult> result = optionals.parallelStream().filter(Optional::isPresent)
+//                .map(Optional::get)
+//                .max(Comparator.comparing(SplitResult::getReduction));
+//
+//        return result;
+//    }
 
 
     public static List<SplitResult> getAllSplits(RegTreeConfig regTreeConfig,
