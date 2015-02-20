ec672075881a1938b378ee36425df8397b1cff34
chengli <chengli.email@gmail.com>
2015-02-20 17:24:55 -0500

    add class Grid

---
+++ src/test/java/edu/neu/ccs/pyramid/util/GridTest.java
@@ -0,0 +1,21 @@
+package edu.neu.ccs.pyramid.util;
+
+import static org.junit.Assert.*;
+
+public class GridTest {
+    public static void main(String[] args) {
+//        test1();
+        test2();
+    }
+
+    private static void test1(){
+        System.out.println(Grid.uniform(0,10,11));
+        System.out.println(Grid.uniform(0,1,5));
+    }
+
+    private static void test2(){
+        System.out.println(Grid.logUniform(0.1,10,3));
+        System.out.println(Grid.logUniform(0.1,1000,5));
+    }
+
+}
