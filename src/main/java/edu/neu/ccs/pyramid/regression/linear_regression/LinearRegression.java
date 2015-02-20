5ba63513ea741a63cf626576a5295f8788808752
chengli <chengli.email@gmail.com>
2015-02-20 17:24:47 -0500

    add serialization and deep copy

--- src/main/java/edu/neu/ccs/pyramid/regression/linear_regression/LinearRegression.java
+++ src/main/java/edu/neu/ccs/pyramid/regression/linear_regression/LinearRegression.java
@@ -3,10 +3,17 @@ package edu.neu.ccs.pyramid.regression.linear_regression;
 import edu.neu.ccs.pyramid.regression.Regressor;
 import org.apache.mahout.math.Vector;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
  * Created by chengli on 2/18/15.
  */
 public class LinearRegression implements Regressor{
+    private static final long serialVersionUID = 1L;
+
     private Weights weights;
 
     public LinearRegression(int numFeatures) {
@@ -27,6 +34,19 @@ public class LinearRegression implements Regressor{
 
     public double predictWithoutBias(Vector vector){
         return this.weights.getWeightsWithoutBias().dot(vector);
+    }
+
+    public LinearRegression deepCopy() throws Exception{
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(this);
+        oos.flush();
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        ObjectInputStream ois = new ObjectInputStream(bais);
+        LinearRegression linearRegression = (LinearRegression) ois.readObject();
+        oos.close();
+        ois.close();
+        return linearRegression;
     }
 
 }
