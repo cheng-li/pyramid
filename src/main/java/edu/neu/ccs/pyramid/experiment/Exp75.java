bea4c7f37f313b090ee394f7f17185be77378316
chengli <chengli.email@gmail.com>
2015-03-26 19:57:42 -0400

    better approximation

--- src/main/java/edu/neu/ccs/pyramid/experiment/Exp75.java
+++ src/main/java/edu/neu/ccs/pyramid/experiment/Exp75.java
@@ -1,8 +1,10 @@
 package edu.neu.ccs.pyramid.experiment;
 
 import edu.neu.ccs.pyramid.classification.PriorProbClassifier;
+import edu.neu.ccs.pyramid.classification.logistic_regression.ElasticNetLogisticTrainer;
 import edu.neu.ccs.pyramid.classification.logistic_regression.LogisticRegression;
 import edu.neu.ccs.pyramid.classification.logistic_regression.LogisticRegressionInspector;
+import edu.neu.ccs.pyramid.classification.logistic_regression.RidgeLogisticTrainer;
 import edu.neu.ccs.pyramid.configuration.Config;
 import edu.neu.ccs.pyramid.dataset.ClfDataSet;
 import edu.neu.ccs.pyramid.dataset.DataSetType;
@@ -19,6 +21,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 /**
+ * approximate logistic gradients
  * Created by chengli on 3/23/15.
  */
 public class Exp75 {
@@ -47,33 +50,53 @@ public class Exp75 {
             String ngram = ((Ngram)feature).getNgram();
             ngramIndexMap.put(ngram,index);
         }
+
+        List<double[]> probs;
+        probs = priorProbClassifier.predictClassProbs(dataSet);
         for (int k=0;k<dataSet.getNumClasses();k++){
+            System.out.println("with prior probabilities:");
             System.out.println("features for class "+k);
-            List<Estimation> estimations = gradientSelection(config,dataSet,k,priorProbClassifier,ngramIndexMap);
+            List<Estimation> estimations = gradientSelection(config,dataSet,k,probs,ngramIndexMap);
             for (Estimation estimation: estimations){
-                System.out.println(estimation);
-
+                if (estimation.getNgram().split(" ").length>1){
+                    System.out.println(estimation);
+                }
             }
-
-
-
+        }
+        LogisticRegression logisticRegression = new LogisticRegression(dataSet.getNumClasses(),dataSet.getNumFeatures());
+        ElasticNetLogisticTrainer trainer = new ElasticNetLogisticTrainer.Builder(logisticRegression,dataSet)
+                .setRegularization(0.00000001)
+                .setL1Ratio(0).build();
+        for (int i = 0;i<5;i++){
+            trainer.iterate();
+            probs = logisticRegression.predictClassProbs(dataSet);
+            System.out.println("after iteration "+i);
+            for (int k=0;k<dataSet.getNumClasses();k++){
+                System.out.println("features for class "+k);
+                List<Estimation> estimations = gradientSelection(config,dataSet,k,probs,ngramIndexMap);
+                for (Estimation estimation: estimations){
+                    if (estimation.getNgram().split(" ").length>1){
+                        System.out.println(estimation);
+                    }
+                }
+            }
         }
 
     }
 
-    private static double utility(ClfDataSet dataSet, int classIndex, int featureIndex, PriorProbClassifier priorProbClassifier){
+    private static double utility(ClfDataSet dataSet, int classIndex, int featureIndex, List<double[]> probs){
         Vector vector = dataSet.getColumn(featureIndex);
         int[] labels = dataSet.getLabels();
         //actual and predicted
         double[] counts = new double[2];
         for (Vector.Element element: vector.nonZeroes()){
             int dataPoint = element.index();
-            counts[1] += 1;
+            counts[1] += probs.get(dataPoint)[classIndex];
             if (labels[dataPoint]==classIndex){
                 counts[0] += 1;
             }
         }
-        return counts[0] - counts[1]*priorProbClassifier.getClassProbs()[classIndex];
+        return counts[0] - counts[1];
     }
 
     //wierd
@@ -100,7 +123,7 @@ public class Exp75 {
         return intersection;
     }
 
-    private static double lowerBound(ClfDataSet dataSet, int classIndex, int featureIndex, PriorProbClassifier priorProbClassifier, Map<String, Integer> ngramIndexMap){
+    private static double lowerBound(ClfDataSet dataSet, int classIndex, int featureIndex, List<double[]> probs, Map<String, Integer> ngramIndexMap){
         Vector vector = dataSet.getColumn(featureIndex);
         int[] labels = dataSet.getLabels();
         //actual and predicted
@@ -112,28 +135,54 @@ public class Exp75 {
                 count += 1;
             }
         }
-        double second = intersection(dataSet,featureIndex,ngramIndexMap).size() * priorProbClassifier.getClassProbs()[classIndex];
+        Set<Integer> intersection = intersection(dataSet,featureIndex,ngramIndexMap);
+        double second = intersection.stream().mapToDouble(i -> probs.get(i)[classIndex]).sum();
         return count - second;
     }
 
-    private static double upperBound(ClfDataSet dataSet, int classIndex, int featureIndex, PriorProbClassifier priorProbClassifier, Map<String, Integer> ngramIndexMap){
+    private static double upperBound(ClfDataSet dataSet, int classIndex, int featureIndex, List<double[]> probs, Map<String, Integer> ngramIndexMap){
         Set<Integer> intersection = intersection(dataSet,featureIndex,ngramIndexMap);
         Set<Integer> filtered = intersection.stream().filter(i -> dataSet.getLabels()[i]==classIndex).collect(Collectors.toSet());
-
-        return filtered.size()*(1-priorProbClassifier.getClassProbs()[classIndex]);
+        double term2 = filtered.stream().mapToDouble(i -> probs.get(i)[classIndex]).sum();
+        return filtered.size() - term2;
     }
 
-    private static List<Estimation> gradientSelection(Config config, ClfDataSet dataSet, int classIndex, PriorProbClassifier priorProbClassifier, Map<String, Integer> ngramIndexMap){
+
+    private static double approximate(ClfDataSet dataSet, int classIndex, int featureIndex, List<double[]> probs, Map<String, Integer> ngramIndexMap){
+        int[] labels = dataSet.getLabels();
+        Set<Integer> intersection = intersection(dataSet,featureIndex,ngramIndexMap);
+        Set<Integer> intersectionK = intersection.stream().filter(i -> labels[i]==classIndex).collect(Collectors.toSet());
+        Set<Integer> intersectionNotK = intersection.stream().filter(i -> labels[i]!=classIndex).collect(Collectors.toSet());
+        double ngramK = 0;
+        double ngramNotK = 0;
+        Vector vector = dataSet.getColumn(featureIndex);
+        for (Vector.Element element: vector.nonZeroes()){
+            int dataIndex = element.index();
+            if (labels[dataIndex]==classIndex){
+                ngramK += 1;
+            } else {
+                ngramNotK += 1;
+            }
+        }
+        double term2 = intersectionK.stream().mapToDouble(i -> probs.get(i)[classIndex]).sum() * ngramK/intersectionK.size();
+        double term3 = intersectionNotK.stream().mapToDouble(i -> probs.get(i)[classIndex]).sum() * ngramNotK/intersectionNotK.size();
+
+        return ngramK - term2 - term3;
+    }
+
+    private static List<Estimation> gradientSelection(Config config, ClfDataSet dataSet, int classIndex, List<double[]> probs, Map<String, Integer> ngramIndexMap){
         int limit = config.getInt("limit");
         List<Estimation> top = dataSet.getFeatureList().getAll().parallelStream().map(feature -> {
             int index = feature.getIndex();
-            double quality = utility(dataSet, classIndex, index, priorProbClassifier);
+            double quality = utility(dataSet, classIndex, index, probs);
             Estimation estimation = new Estimation();
             estimation.setGradient(quality);
-            double lower = lowerBound(dataSet, classIndex, index, priorProbClassifier, ngramIndexMap);
-            double upper = upperBound(dataSet, classIndex, index, priorProbClassifier, ngramIndexMap);
+            double lower = lowerBound(dataSet, classIndex, index, probs, ngramIndexMap);
+            double upper = upperBound(dataSet, classIndex, index, probs, ngramIndexMap);
+            double approximate = approximate(dataSet, classIndex, index, probs, ngramIndexMap);
             estimation.setLower(lower);
             estimation.setUpper(upper);
+            estimation.setApproximate(approximate);
             String ngram = ((Ngram)feature).getNgram();
             estimation.setNgram(ngram);
             return estimation;
@@ -148,6 +197,8 @@ public class Exp75 {
         private double gradient;
         private double lower;
         private double upper;
+        private double approximate;
+
 
         public double getGradient() {
             return gradient;
@@ -181,11 +232,22 @@ public class Exp75 {
             this.ngram = ngram;
         }
 
+        public double getApproximate() {
+            return approximate;
+        }
+
+        public void setApproximate(double approximate) {
+            this.approximate = approximate;
+        }
+
+
+
         @Override
         public String toString() {
             final StringBuilder sb = new StringBuilder();
-            sb.append(ngram).append('\t');
+            sb.append(ngram).append("\t\t\t");
             sb.append(gradient).append('\t');
+            sb.append(approximate).append('\t');
             sb.append(lower).append('\t');
             sb.append(upper).append('\t');
             return sb.toString();
