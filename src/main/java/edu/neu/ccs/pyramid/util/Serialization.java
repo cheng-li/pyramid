028132156017b3526be21214083bfea5b162c0e7
chengli <chengli.email@gmail.com>
2015-04-04 13:15:27 -0400

    add class Serialization

---
+++ src/main/java/edu/neu/ccs/pyramid/util/Serialization.java
@@ -0,0 +1,44 @@
+package edu.neu.ccs.pyramid.util;
+
+import java.io.*;
+
+/**
+ * Created by chengli on 4/4/15.
+ */
+public class Serialization {
+
+    public static void serialize(Object object, File file) throws Exception{
+        File parent = file.getParentFile();
+        if (!parent.exists()){
+            parent.mkdirs();
+        }
+        try (
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+                BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(bufferedOutputStream);
+        ){
+            objectOutputStream.writeObject(object);
+        }
+    }
+
+    public static void serialize(Object object, String file) throws Exception{
+        serialize(object,new File(file));
+    }
+
+
+    public static Object deserialize(File file) throws Exception{
+        try(
+                FileInputStream fileInputStream = new FileInputStream(file);
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+                ObjectInputStream objectInputStream = new ObjectInputStream(bufferedInputStream);
+        ){
+            return objectInputStream.readObject();
+        }
+    }
+
+    public static Object deserialize(String file) throws Exception{
+        return deserialize(new File(file));
+    }
+
+
+}
