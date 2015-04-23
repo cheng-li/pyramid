baf604a49c8244ec7699fab22dcc16fbaaebee61
Rainicy <rainicy925@gmail.com>
2015-04-23 19:20:34 -0400

    index congressional bill.

--- src/main/java/edu/neu/ccs/pyramid/data_formatter/congressional_bill/IndexBuilder.java
+++ src/main/java/edu/neu/ccs/pyramid/data_formatter/congressional_bill/IndexBuilder.java
@@ -1,9 +1,16 @@
 package edu.neu.ccs.pyramid.data_formatter.congressional_bill;
 
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Created by Rainicy on 4/23/15.
@@ -14,12 +21,79 @@ public class IndexBuilder {
 
 
 
+
+    public static XContentBuilder getBuilder(String docno, String label, String billText, String featureList) throws IOException {
+
+        // parsing billText into text and title string from JSON format
+        // parsing featureList into feature field string from JSON format
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode actualObj = mapper.readValue(billText, JsonNode.class);
+        String title = "";
+        String body = "";
+
+        try {
+            title = actualObj.get("title").asText();
+            body = actualObj.get("text").asText();
+        } catch(NullPointerException e) {
+            System.out.println("Title or text does not exsit.");
+        }
+
+        actualObj = mapper.readValue(featureList, JsonNode.class);
+        Iterator<String> iterator = actualObj.fieldNames();
+        Set<String> expectedFields = new HashSet<>();
+        while (iterator.hasNext()) {
+            String field = iterator.next();
+            if (acceptField(field)) {
+                expectedFields.add(field);
+            }
+        }
+
+        XContentBuilder builder = getBuilder(docno, label, title, body, expectedFields);
+
+        return builder;
+
+
+    }
+
+    private static XContentBuilder getBuilder(String docno, String label, String title, String body, Set<String> expectedFields) throws IOException {
+
+        XContentBuilder builder = XContentFactory.jsonBuilder();
+
+        builder.startObject();
+        builder.field("docno", docno);
+        builder.field("label", label);
+        builder.field("title", title);
+        builder.field("body", body);
+
+        for (String field : expectedFields) {
+            builder.field(field, "1");
+        }
+
+        builder.endObject();
+        return builder;
+    }
+
+    /**
+     * if the given field is the expected field.
+     * @param field
+     * @return
+     */
+    public static boolean acceptField(String field) {
+
+        if (field.contains("bill_cat4-function")) return false;
+        if (field.contains("sim_")) return false;
+        if (field.contains("body-word_")) return false;
+        if (field.contains("title-word_")) return false;
+
+        return true;
+    }
+
     /**
      * Create the builder for mapping.
      * @return
      * @throws Exception
      */
-    public static XContentBuilder createMapping() throws Exception {
+    public static XContentBuilder createMapping(Set<String> featureFields) throws Exception {
         // mapping the defualt fileds: document number and text.
         XContentBuilder mapping = XContentFactory.jsonBuilder()
                 .startObject()
@@ -43,8 +117,24 @@ public class IndexBuilder {
                 .field("index", "analyzed")
                 .field("term_vector", "with_positions")
                 .field("analyzer", "my_english")
-                .endObject()
-                .endObject()
+                .endObject();
+
+        for(String feature : featureFields) {
+            mapping.startObject(feature)
+                    .field("type", "string")
+                    .field("store", true)
+                    .field("index", "not_analyzed")
+                    .field("null_value", "0")
+                    .endObject();
+        }
+
+        mapping.startObject("label")
+                .field("type", "string")
+                .field("store", true)
+                .field("index", "not_analyzed")
+                .endObject();
+
+        mapping.endObject()
                 .endObject()
                 .endObject();
 
---
+++ src/main/java/edu/neu/ccs/pyramid/experiment/Exp87.java
@@ -0,0 +1,133 @@
+package edu.neu.ccs.pyramid.experiment;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.neu.ccs.pyramid.configuration.Config;
+import edu.neu.ccs.pyramid.util.DirWalker;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.*;
+
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.node.Node;
+
+import static edu.neu.ccs.pyramid.data_formatter.congressional_bill.IndexBuilder.acceptField;
+import static edu.neu.ccs.pyramid.data_formatter.congressional_bill.IndexBuilder.createMapping;
+import static edu.neu.ccs.pyramid.data_formatter.congressional_bill.IndexBuilder.getBuilder;
+import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
+
+/**
+ * Created by Rainicy on 4/23/15.
+ */
+public class Exp87 {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            throw new IllegalArgumentException("please specify the config file.");
+        }
+
+        Config config = new Config(args[0]);
+        System.out.println(config);
+
+
+        // Step 1: getting response/labels and feature list
+        System.out.println("Step 1: getting response/labels and feature list.");
+        Map<String, String> mapperLabels = new HashMap<>();
+        Map<String, String> mapperFeatures = new HashMap<>();
+        Set<String> expectedFields = new HashSet<>();
+
+        String responseFile = config.getString("index.responseFile");
+        String featureFile = config.getString("index.featureFile");
+        String folder = config.getString("index.folder");
+        List<File> allFiles = DirWalker.getFiles(folder);
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode actualObj;
+
+        for (File f : allFiles) {
+            String fName = f.getName();
+            // response file
+            if (fName.equals(responseFile)) {
+                System.out.println("-- dealing with response file: " + f);
+                BufferedReader br = new BufferedReader(new FileReader(f));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    String[] docnoLabel = line.split("\\t");
+                    mapperLabels.put(docnoLabel[0], docnoLabel[1]);
+                }
+                br.close();
+            } else if (fName.equals(featureFile)) {
+                System.out.println("-- dealing with feature file: " + f);
+                BufferedReader br = new BufferedReader(new FileReader(f));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    String[] docnoFeature = line.split("\\t");
+                    mapperFeatures.put(docnoFeature[0], docnoFeature[1]);
+
+                    // getting expected features
+                    actualObj = mapper.readValue(docnoFeature[1], JsonNode.class);
+                    Iterator<String> iterator = actualObj.fieldNames();
+                    while (iterator.hasNext()) {
+                        String field = iterator.next();
+                        if (acceptField(field)) {
+                            expectedFields.add(field);
+                        }
+                    }
+                }
+                br.close();
+            }
+        }
+        System.out.println("number of fields: " + expectedFields.size());
+        System.out.println("enter return to continue ......");
+        System.in.read();
+
+        // Step 2: Mapping
+        System.out.println("Step 2: Mapping ...");
+        Node node = nodeBuilder().client(true).clusterName(config.getString("index.clusterName")).node();
+        Client client = node.client();
+        String indexName = config.getString("index.name");
+        String indexDocumentType = config.getString("index.documentType");
+
+        // set mapping
+        XContentBuilder mappingBuilder = createMapping(expectedFields);
+        client.admin().indices().preparePutMapping(indexName)
+                .setType(indexDocumentType)
+                .setSource(mappingBuilder)
+                .execute()
+                .actionGet();
+
+
+        // Step 3: getting the original text file and indexing
+        System.out.println("Step 3: getting the original text file and indexing ...");
+        int id = 0;
+
+        String textFile = config.getString("index.textFile");
+        BufferedReader br = new BufferedReader(new FileReader(textFile));
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println("doc id: " + id);
+            String[] docnoText = line.split("\\t");
+            String docno = docnoText[0];
+            String text = docnoText[1];
+            String label = mapperLabels.get(docno);
+            String feature = mapperFeatures.get(docno);
+            XContentBuilder builder = getBuilder(docno, label, text, feature);
+
+            IndexResponse response = client.prepareIndex(indexName, indexDocumentType, ""+id)
+                    .setSource(builder)
+                    .execute()
+                    .actionGet();
+            ++id;
+        }
+        br.close();
+
+        // close node
+        node.close();
+
+    }
+}
