606b648c32ba6087ba361924d7562e036a37f1c8
Rainicy <rainicy925@gmail.com>
2015-09-07 19:30:24 -0400

    GibbsSampling for MultiLabel classification.

---
+++ src/main/java/edu/neu/ccs/pyramid/experiment/Exp207.java
@@ -0,0 +1,7 @@
+package edu.neu.ccs.pyramid.experiment;
+
+/**
+ * Created by Rainicy on 9/7/15.
+ */
+public class Exp207 {
+}
---
+++ src/main/java/edu/neu/ccs/pyramid/multilabel_classification/sampling/GibbsSampling.java
@@ -0,0 +1,7 @@
+package edu.neu.ccs.pyramid.multilabel_classification.sampling;
+
+/**
+ * Created by Rainicy on 9/7/15.
+ */
+public class GibbsSampling {
+}
