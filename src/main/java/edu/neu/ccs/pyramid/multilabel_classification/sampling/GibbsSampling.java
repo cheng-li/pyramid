e98372b4546da5e6b801a420c33c7b6844c0d9a2
Rainicy <rainicy925@gmail.com>
2015-09-07 19:38:47 -0400

    add GibbsSampling for multi label classification.

--- src/main/java/edu/neu/ccs/pyramid/multilabel_classification/sampling/GibbsSampling.java
+++ src/main/java/edu/neu/ccs/pyramid/multilabel_classification/sampling/GibbsSampling.java
@@ -1,7 +1,203 @@
 package edu.neu.ccs.pyramid.multilabel_classification.sampling;
 
+import edu.neu.ccs.pyramid.classification.Classifier;
+import edu.neu.ccs.pyramid.dataset.LabelTranslator;
+import edu.neu.ccs.pyramid.dataset.MultiLabel;
+import edu.neu.ccs.pyramid.feature.FeatureList;
+import edu.neu.ccs.pyramid.multilabel_classification.MultiLabelClassifier;
+import org.apache.mahout.math.DenseVector;
+import org.apache.mahout.math.SequentialAccessSparseVector;
+import org.apache.mahout.math.Vector;
+
+import java.util.*;
+
 /**
  * Created by Rainicy on 9/7/15.
  */
-public class GibbsSampling {
+public class GibbsSampling implements MultiLabelClassifier {
+
+    private List<Classifier.ProbabilityEstimator> classifiers;
+    private int numClasses;
+
+    // gibbs sampling k times.
+    private int K;
+
+    // feature list and labelTranslator, optional
+    private FeatureList featureList;
+    private LabelTranslator labelTranslator;
+
+
+    public GibbsSampling(int numClasses, int K) {
+        this.numClasses = numClasses;
+        this.K = K;
+        for (int i=0; i<numClasses; i++) {
+            this.classifiers = new ArrayList<>(numClasses);
+        }
+    }
+
+    public void addClassifier(Classifier.ProbabilityEstimator classifier) {
+        this.classifiers.add(classifier);
+    }
+
+    /**
+     * Returns the number of classes.
+     * @return
+     */
+    public int getNumClasses() {
+        return this.numClasses;
+    }
+
+    @Override
+    public MultiLabel predict(Vector vector) {
+
+        // record the votes at the end of each gibbs sampling iteration.
+        // get the count for each combination of labels(MultiLabel).
+        Map<MultiLabel, Integer> votes = new HashMap<>();
+
+        // append the (#labels-1) as features to the original vector
+        Vector toVector = extendFeatures(vector);
+        // initial current label features value
+        int[] labelFeatures = new int[numClasses];
+
+
+        // starting gibbs sampling
+        for (int i=0; i<K; i++) {
+            // for each gibbs sampling iteration, go through all classifiers
+            for (int j=0; j<numClasses; j++) {
+                // binary classifier.
+                Classifier.ProbabilityEstimator classifier = classifiers.get(j);
+                // the probability of predicting 1.
+                double prob = classifier.predictClassProbs(toVector)[1];
+                // update if the current label is on or off.
+                labelFeatures[j] = flipCoin(prob);
+                // update the toVector for next classifier use.
+                updateVector(toVector, labelFeatures, j);
+            }
+
+            // record current iteration of prediction at the last 50 itertaions
+            if ((K-i) <= 50) {
+                MultiLabel multiLabel = transMultiLabel(labelFeatures);
+                if (!votes.containsKey(multiLabel)) {
+                    votes.put(multiLabel, 1);
+                } else {
+                    votes.put(multiLabel, votes.get(multiLabel)+1);
+                }
+            }
+        }
+
+        // predict the majority of the MultiLabel in votes.
+        MultiLabel predLabel = null;
+        int maxVoteCount = Integer.MIN_VALUE;
+        for (HashMap.Entry<MultiLabel, Integer> vote : votes.entrySet()) {
+            int voteCount = vote.getValue();
+            if (maxVoteCount < voteCount) {
+                maxVoteCount = voteCount;
+                predLabel = vote.getKey();
+            }
+        }
+
+        return predLabel;
+    }
+
+    /**
+     * By given the array of label features, if the label's value=1, then turn on
+     * this label, otherwise off.
+     * @param labelFeatures
+     * @return
+     */
+    private MultiLabel transMultiLabel(int[] labelFeatures) {
+        MultiLabel multiLabel = new MultiLabel();
+
+        for (int i=0; i<labelFeatures.length; i++) {
+            if (labelFeatures[i] == 1) {
+                multiLabel.addLabel(i);
+            }
+        }
+        return multiLabel;
+    }
+
+    /**
+     * update the given toVector based on given label features.
+     * @param toVector
+     * @param labelFeatures
+     * @param j
+     */
+    private void updateVector(Vector toVector, int[] labelFeatures, int j) {
+
+        int vectorIndex = numClasses;
+        for (int i=0; i<labelFeatures.length; i++) {
+            if (i == j) {
+                continue;
+            }
+            toVector.set(vectorIndex++, labelFeatures[i]);
+        }
+    }
+
+    /**
+     * given the probability of header of a coin, simulate the
+     * result of flipping the coin.
+     * For example, given prob = 0.6: if generated prob = 0.3, then
+     * return 1; if generated prob = 0.9, then return 0.
+     * @param prob
+     * @return 1 if generated prob is smaller than given prob; otherwise 0.
+     */
+    private int flipCoin(double prob) {
+        // if the random probability is smaller than given prob,
+        // return true.
+        if (Math.random() < prob) {
+            return 1;
+        }
+        return 0;
+    }
+
+    /**
+     * By given a vector and number of classes, append (#classes-1) new features
+     * to the original vector. The default new features values are 0.
+     * @param vector the original features vector.
+     * @return the extended vector.
+     */
+    private Vector extendFeatures(Vector vector) {
+        Vector toVector;
+        int newLength = vector.size() + numClasses - 1;
+
+        // initialize the new vector
+        if (vector.isDense()) {
+            toVector = new DenseVector(newLength);
+        } else {
+            toVector = new SequentialAccessSparseVector(newLength);
+        }
+
+        // copy the original vector to the new vector
+        for (Vector.Element element : vector.nonZeroes()) {
+            int index = element.index();
+            double value = element.get();
+            toVector.set(index, value);
+        }
+        // append the label value as 0 to the new vector.
+        for (int i=vector.size(); i<newLength; i++) {
+            toVector.set(i, 0);
+        }
+
+
+        return toVector;
+    }
+
+
+    @Override
+    public FeatureList getFeatureList() {
+        return featureList;
+    }
+
+    @Override
+    public LabelTranslator getLabelTranslator() {
+        return labelTranslator;
+    }
+
+    public void setFeatureList(FeatureList featureList) {
+        this.featureList = featureList;
+    }
+
+    public void setLabelTranslator(LabelTranslator labelTranslator) {
+        this.labelTranslator = labelTranslator;
+    }
 }
--- src/main/java/edu/neu/ccs/pyramid/multilabel_classification/sampling/GibbsSamplingConfig.java
+++ src/main/java/edu/neu/ccs/pyramid/multilabel_classification/sampling/GibbsSamplingConfig.java
@@ -1,7 +1,219 @@
 package edu.neu.ccs.pyramid.multilabel_classification.sampling;
 
+import edu.neu.ccs.pyramid.dataset.MultiLabelClfDataSet;
+import edu.neu.ccs.pyramid.regression.regression_tree.LeafOutputType;
+import edu.neu.ccs.pyramid.util.Sampling;
+
+import java.util.stream.IntStream;
+
 /**
  * Created by Rainicy on 9/7/15.
  */
 public class GibbsSamplingConfig {
+
+    private MultiLabelClfDataSet dataSet;
+
+    private double learningRate;
+    private int numLeaves;
+    private int minDataPerLeaf;
+    private int[] activeFeatures;
+    private int[] activeDataPoints;
+    private int numSplitIntervals;
+    private int randomLevel;
+    private LeafOutputType leafOutputType;
+    private boolean considerHardTree;
+    private boolean considerExpectationTree;
+    private boolean considerProbabilisticTree;
+    private int numRounds;
+
+    // private
+    public GibbsSamplingConfig(Builder builder) {
+        this.dataSet = builder.dataSet;
+        this.learningRate = builder.learningRate;
+        this.numLeaves = builder.numLeaves;
+        this.minDataPerLeaf = builder.minDataPerLeaf;
+        double dataSamplingRate = builder.dataSamplingRate;
+        double featureSamplingRate = builder.featureSamplingRate;
+        this.numSplitIntervals = builder.numSplitIntervals;
+        this.randomLevel = builder.randomLevel;
+        int numDataPoints = dataSet.getNumDataPoints();
+        this.leafOutputType = builder.leafOutputType;
+        this.considerHardTree=builder.considerHardTree;
+        this.considerExpectationTree=builder.considerExpectationTree;
+        this.considerProbabilisticTree=builder.considerProbabilisticTree;
+        this.numRounds=builder.numRounds;
+        if (dataSamplingRate == 1) {
+            /**
+             * preserve orders (seems does not matter for data)
+             */
+            this.activeDataPoints = IntStream.range(0, numDataPoints).toArray();
+        } else {
+            /**
+             * does not preserve orders
+             */
+            this.activeDataPoints = Sampling.sampleByPercentage(numDataPoints,
+                    dataSamplingRate);
+        }
+
+        if (featureSamplingRate == 1) {
+            /**
+             * preserve orders
+             */
+            this.activeFeatures = IntStream.range(0, this.dataSet.getNumFeatures())
+                    .toArray();
+        } else {
+            /**
+             * does not preserve orders
+             */
+            this.activeFeatures = Sampling.sampleByPercentage(this.dataSet.getNumFeatures(),
+                    featureSamplingRate);
+        }
+    }
+
+    MultiLabelClfDataSet getDataSet() {
+        return dataSet;
+    }
+
+    public double getLearningRate() {
+        return learningRate;
+    }
+
+    public int getNumLeaves() {
+        return numLeaves;
+    }
+
+    public int getMinDataPerLeaf() {
+        return minDataPerLeaf;
+    }
+
+    public int[] getActiveFeatures() {
+        return activeFeatures;
+    }
+
+    public int[] getActiveDataPoints() {
+        return activeDataPoints;
+    }
+
+    public int getNumSplitIntervals() {
+        return numSplitIntervals;
+    }
+
+    public int getRandomLevel() {
+        return randomLevel;
+    }
+
+    public LeafOutputType getLeafOutputType() {
+        return leafOutputType;
+    }
+
+    public boolean isConsiderHardTree() {
+        return considerHardTree;
+    }
+
+    public boolean isConsiderExpectationTree() {
+        return considerExpectationTree;
+    }
+
+    public boolean isConsiderProbabilisticTree() {
+        return considerProbabilisticTree;
+    }
+
+    public int getNumRounds() {
+        return numRounds;
+    }
+
+
+    public static class Builder {
+        /**
+         * required
+         */
+        private MultiLabelClfDataSet dataSet;
+
+        /**
+         * optional
+         */
+        int numRounds = 100;
+        int numLeaves = 2;
+        double learningRate = 1;
+        int minDataPerLeaf = 1;
+        double dataSamplingRate=1;
+        double featureSamplingRate=1;
+        private int numSplitIntervals =100;
+        private int randomLevel =1;
+        private LeafOutputType leafOutputType = LeafOutputType.NEWTON;
+        private boolean considerHardTree=true;
+        private boolean considerExpectationTree=false;
+        private boolean considerProbabilisticTree=false;
+
+        public Builder(MultiLabelClfDataSet dataSet) {
+            this.dataSet = dataSet;
+        }
+
+        public Builder numRounds(int numRounds) {
+            this.numRounds = numRounds;
+            return this;
+        }
+
+        public Builder numLeaves(int numLeaves){
+            this.numLeaves = numLeaves;
+            return this;
+        }
+
+        public Builder learningRate(double learningRate) {
+            this.learningRate = learningRate;
+            return this;
+        }
+
+        public Builder minDataPerLeaf(int minDataPerLeaf) {
+            this.minDataPerLeaf = minDataPerLeaf;
+            return this;
+        }
+
+
+        public Builder dataSamplingRate(double dataSamplingRate) {
+            this.dataSamplingRate = dataSamplingRate;
+            return this;
+        }
+
+        public Builder featureSamplingRate(double featureSamplingRate) {
+            this.featureSamplingRate = featureSamplingRate;
+            return this;
+        }
+
+        public Builder numSplitIntervals(int numSplitIntervals) {
+            this.numSplitIntervals = numSplitIntervals;
+            return this;
+        }
+
+
+        public Builder randomLevel(int randomLevel){
+            this.randomLevel = randomLevel;
+            return this;
+        }
+
+        public Builder setLeafOutputType(LeafOutputType leafOutputType) {
+            this.leafOutputType = leafOutputType;
+            return this;
+        }
+
+
+        public Builder considerHardTree(boolean considerHardTree) {
+            this.considerHardTree = considerHardTree;
+            return this;
+        }
+
+        public Builder considerExpectationTree(boolean considerExpectationTree) {
+            this.considerExpectationTree = considerExpectationTree;
+            return this;
+        }
+
+        public Builder considerProbabilisticTree(boolean considerProbabilisticTree) {
+            this.considerProbabilisticTree = considerProbabilisticTree;
+            return this;
+        }
+
+        public GibbsSamplingConfig build() {
+            return new GibbsSamplingConfig(this);
+        }
+    }
 }
