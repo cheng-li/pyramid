5d41a246a921a936ce4e669fabea9034e257693a
Henry-Yan <henryjl.yan@gmail.com>
2015-09-25 20:30:10 -0700

    integrated sentences to vectors

---
+++ src/main/java/edu/neu/ccs/pyramid/experiment/Exp300.java
@@ -0,0 +1,273 @@
+package edu.neu.ccs.pyramid.experiment;
+
+import edu.neu.ccs.pyramid.configuration.Config;
+import edu.neu.ccs.pyramid.dataset.ClfDataSet;
+import edu.neu.ccs.pyramid.dataset.ClfDataSetBuilder;
+import edu.neu.ccs.pyramid.dataset.TRECFormat;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.node.Node;
+import org.elasticsearch.search.SearchHit;
+
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
+
+/**
+ * Created by Jilin on 9/12/15.
+ */
+class Document {
+    public String id;
+    public String body;
+    public String label;
+    public String split;
+
+    public Document(String id, String body, String label, String split) {
+        this.id = id;
+        this.body = body;
+        this.label = label;
+        this.split = split;
+    }
+
+    public String toString() {
+        return "Document:\n"
+                + "\tid: " + this.id + "\n"
+                + "\tbody: " + this.body + "\n"
+                + "\tlabel: " + this.label + "\n"
+                + "\tsplit: " + this.split;
+    }
+}
+
+
+public class Exp300 {
+
+    static Config config = null;
+    static int numberOfTrainingPoints = 0;
+    static int numberOfTestingPoints = 0;
+
+    public static void main(String[] args) throws Exception {
+        if (args.length !=1){
+            throw new IllegalArgumentException("Please specify a properties file.");
+        }
+        config = new Config(args[0]);
+        System.out.println(config);
+
+        if (config.getBoolean("es.on")) {
+            // Get sentences and labels from ElasticSearch.
+            System.out.println("Reading documents from Elastic Search...");
+            getSentencesAndLabelsFromElasticSearch();
+            System.out.println("The sentences and labels have been written into files!");
+        }
+
+        if (config.getBoolean("word2vec.on")) {
+            // Train word vectors using the sentences and labels
+            System.out.println("Start to train word vectors...");
+            wordsToVectors(Paths.get(args[0]).toAbsolutePath().normalize().toString());
+            System.out.println("Finish to train word vectors!");
+        }
+
+        if (config.getBoolean("sent2vec.on")) {
+            // Train sentence vectors for training and testing set
+            System.out.println("Start to train sentence vectors...");
+            sentencesToVectors(Paths.get(args[0]).toAbsolutePath().normalize().toString());
+            System.out.println("Finish to train sentence vectors!");
+        }
+
+        // Populate the vectors and labels into trec files
+        // TODO: numberOfTrainPoints and numberOfTestPoints
+        System.out.println("Writing vectors and labels into Trec files...");
+        writeTrec(numberOfTrainingPoints, "/sentence.vector/train.vectors", "/documents/train.labels", "/trec/train.trec");
+        writeTrec(numberOfTestingPoints, "/sentence.vector/test.vectors", "/documents/test.labels", "/trec/test.trec");
+        System.out.println("Finished!");
+    }
+
+    /**
+     * Get all of the documents in Elastic Search with the given configuration
+     * @return a list of documents
+     */
+    static ArrayList<Document> getAllDocuments() {
+        String clusterName = config.getString("es.clusterName");
+        String indexName = config.getString("es.indexName");
+        String documentType = config.getString("es.documentType");
+
+        // Split the given labels and populate them into a HashMap
+        int numberOfClasses = config.getInt("es.numberOfClasses");
+        HashMap<String, String> labelMap = new HashMap<>();
+        for (int i = 0; i < numberOfClasses; i++) {
+            labelMap.put(config.getString("es.label" + Integer.toString(i)), Integer.toString(i));
+        }
+
+        Node node = nodeBuilder().client(true).clusterName(clusterName).node();
+        Client client = node.client();
+
+        // Get all the documents and add them into the list.
+        ArrayList<Document> documents = new ArrayList<>();
+        int scrollSize = 1000, counter = 0;
+        SearchResponse response = null;
+        while (response == null || response.getHits().hits().length != 0) {
+            response = client.prepareSearch(indexName)
+                    .setTypes(documentType)
+                    .setFrom(counter * scrollSize)
+                    .setSize(scrollSize)
+                    .execute()
+                    .actionGet();
+
+            for (SearchHit hit : response.getHits()) {
+                String id = hit.getId();
+                Map<String, Object> source = hit.getSource();
+                String body = (String)source.get("body");
+                String label = source.get("label").toString();
+                String split = (String)source.get("split");
+
+                if (labelMap.containsKey(label)) {
+                    documents.add(new Document(id, body, labelMap.get(label), split));
+                }
+            }
+            counter++;
+        }
+
+        node.close();
+        return documents;
+    }
+
+    static BufferedWriter getBufferedWriter(String outputFolder, String filePath) throws IOException {
+        File file = new File(outputFolder, filePath);
+        file.getParentFile().mkdirs();
+        return new BufferedWriter(new FileWriter(file));
+    }
+
+    static BufferedReader getBufferedReader(String outputFolder, String filePath) throws IOException {
+        File file = new File(outputFolder, filePath);
+        return new BufferedReader(new FileReader(file));
+    }
+
+    /**
+     * Get all of the documents in Elastic Search, and split them into two
+     * sets: train and test. Then write the sentences and labels into files.
+     */
+    static void getSentencesAndLabelsFromElasticSearch() throws IOException {
+        ArrayList<Document> documents = getAllDocuments();
+
+        String outputFolder = config.getString("output.folder");
+        BufferedWriter trainSentencesWriter = getBufferedWriter(outputFolder, "/documents/train.sentences");
+        BufferedWriter trainLabelsWriter = getBufferedWriter(outputFolder, "/documents/train.labels");
+        BufferedWriter testSentencesWriter = getBufferedWriter(outputFolder, "/documents/test.sentences");
+        BufferedWriter testLabelsWriter = getBufferedWriter(outputFolder, "/documents/test.labels");
+
+        // Shuffle the list and split the documents into two sets according to the given percentage.
+        // Otherwise, just split them into two sets according to the split field.
+        if (config.getBoolean("es.shuffle")) {
+            Collections.shuffle(documents);
+            int totalNumber = documents.size();
+            numberOfTrainingPoints = (int)(totalNumber * config.getDouble("es.trainPercentage"));
+            numberOfTestingPoints = totalNumber - numberOfTrainingPoints;
+            for (int i = 0; i < numberOfTrainingPoints; i++) {
+                trainSentencesWriter.write(documents.get(i).body + "\n");
+                trainLabelsWriter.write(documents.get(i).label + "\n");
+            }
+            for (int i = numberOfTrainingPoints; i < totalNumber; i++) {
+                testSentencesWriter.write(documents.get(i).body + "\n");
+                testLabelsWriter.write(documents.get(i).label + "\n");
+            }
+        } else {
+            for (Document doc : documents) {
+                if (doc.split.equals("train")) {
+                    trainSentencesWriter.write(doc.body + "\n");
+                    trainLabelsWriter.write(doc.label + "\n");
+                    numberOfTrainingPoints++;
+                } else {
+                    testSentencesWriter.write(doc.body + "\n");
+                    testLabelsWriter.write(doc.label + "\n");
+                    numberOfTestingPoints++;
+                }
+            }
+        }
+
+        trainSentencesWriter.close();
+        trainLabelsWriter.close();
+        testSentencesWriter.close();
+        testLabelsWriter.close();
+    }
+
+    /**
+     * Execute the given command line and print out the outputs or errors
+     * @param command given command line
+     * @throws IOException
+     */
+    static void exec(String command) throws IOException {
+        Process proc = Runtime.getRuntime().exec(command);
+
+        String line = null;
+        // read the output from the command
+        BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        while ((line = stdInput.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        // read any errors from the attempted command1
+        BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+        while ((line = stdError.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+
+    /**
+     * Execute python methods to train word vectors using the given options
+     */
+    static void wordsToVectors(String configFilePath) throws IOException {
+        String pythonFolder = config.getString("python.folder");
+        String command = String.format("python %s/word_to_vector.py %s", pythonFolder, configFilePath);
+        exec(command);
+    }
+
+    /**
+     * Execute python methods to train sentence vectors using the given options
+     */
+    static void sentencesToVectors(String configFilePath) throws IOException {
+        String pythonFolder = config.getString("python.folder");
+        String command = String.format("python %s/sentence_to_vector.py %s", pythonFolder, configFilePath);
+        exec(command);
+    }
+
+    /**
+     *  Populate the given vectors and labels into trec files
+     */
+    static void writeTrec(int numberOfPoints, String vectorsFilePath,
+                          String labelsFilePath, String trecFilePath) throws Exception {
+        String outputFolder = config.getString("output.folder");
+        int numberOfFeatures = config.getInt("word2vec.size");
+        int numberOfClasses = config.getInt("es.numberOfClasses");
+
+        ClfDataSet dataSet = ClfDataSetBuilder.getBuilder()
+                .numDataPoints(numberOfPoints)
+                .numFeatures(numberOfFeatures)
+                .numClasses(numberOfClasses)
+                .dense(true)
+                .missingValue(false)
+                .build();
+
+        BufferedReader trainReader = getBufferedReader(outputFolder, vectorsFilePath);
+        BufferedReader labelReader = getBufferedReader(outputFolder, labelsFilePath);
+        for (int i = 0; i < numberOfPoints; i++) {
+            String line = trainReader.readLine();
+            String[] features = line.split(" ");
+            if (features.length != numberOfFeatures) {
+                throw new Exception("The number of features is invalid");
+            }
+
+            for (int j = 0; j < numberOfFeatures; j++) {
+                dataSet.setFeatureValue(i, j, Double.valueOf(features[j]));
+            }
+
+            int label = Integer.valueOf(labelReader.readLine());
+            dataSet.setLabel(i, label);
+        }
+
+        TRECFormat.save(dataSet, new File(outputFolder, trecFilePath));
+    }
+}
---
+++ src/main/python/sentence_to_vector.py
@@ -0,0 +1,68 @@
+import logging, sys, os, numpy, ConfigParser
+from word2vec import Word2Vec, Sent2Vec, LineSentence
+
+logging.basicConfig(format='%(asctime)s : %(threadName)s : %(levelname)s : %(message)s',
+                    level=logging.INFO,
+                    stream=sys.stdout)
+logging.info("running %s" % " ".join(sys.argv))
+
+# Read the configuration file
+config = ConfigParser.ConfigParser()
+config.read(sys.argv[1])
+
+# Get the output directory and file paths
+if config.has_option('Global', 'output.folder'):
+    output_folder = config.get('Global', 'output.folder')
+    train_sentences_path = os.path.join(output_folder, 'documents/train.sentences')
+    test_sentences_path = os.path.join(output_folder, 'documents/test.sentences')
+    if not os.path.isfile(train_sentences_path) or not os.path.isfile(test_sentences_path):
+        logging.error("can't find training or testing sentences")
+        sys.exit(0)
+
+    word_vectors_model_path = os.path.join(output_folder, 'word.vector/word.vectors.model')
+    if not os.path.isfile(word_vectors_model_path):
+        logging.error("can't find the model file of word vector")
+        sys.exit(0)
+
+    # Create a directory and indicate file paths to store sentence vectors and the model
+    if not os.path.exists(os.path.join(output_folder, 'sentence.vector')):
+        os.makedirs(os.path.join(output_folder, 'sentence.vector'))
+    train_vectors_path = os.path.join(output_folder, 'sentence.vector/train.vectors')
+    test_vectors_path = os.path.join(output_folder, 'sentence.vector/test.vectors')
+else:
+    logging.error("please indicate the output folder in configuration file")
+    sys.exit(0)
+
+
+# Get the options from configuration file
+if config.has_section('SentenceToVector'):
+    alpha = config.getfloat('SentenceToVector', 'sent2vec.alpha')
+    window = config.getint('SentenceToVector', 'sent2vec.window')
+    sample = config.getfloat('SentenceToVector', 'sent2vec.sample')
+    seed = config.getint('SentenceToVector', 'sent2vec.seed')
+    workers = config.getint('SentenceToVector', 'sent2vec.workers')
+    min_alpha = config.getfloat('SentenceToVector', 'sent2vec.min_alpha')
+    sg = config.getint('SentenceToVector', 'sent2vec.sg')
+    hs = config.getint('SentenceToVector', 'sent2vec.hs')
+    negative = config.getint('SentenceToVector', 'sent2vec.negative')
+    cbow_mean = config.getint('SentenceToVector', 'sent2vec.cbow_mean')
+    iteration = config.getint('SentenceToVector', 'sent2vec.iteration')
+
+    # Train sentence vectors on training and testing sets
+    train_model = Sent2Vec(LineSentence(train_sentences_path), model_file=word_vectors_model_path,
+                           alpha=alpha, window=window, sample=sample, seed=seed, workers=workers,min_alpha=min_alpha,
+                           sg=sg, hs=hs, negative=negative, cbow_mean=cbow_mean, iteration=iteration)
+    test_model = Sent2Vec(LineSentence(test_sentences_path), model_file=word_vectors_model_path,
+                          alpha=alpha, window=window, sample=sample, seed=seed, workers=workers,min_alpha=min_alpha,
+                          sg=sg, hs=hs, negative=negative, cbow_mean=cbow_mean, iteration=iteration)
+else:
+    # Train sentence vectors on training and testing sets
+    train_model = Sent2Vec(LineSentence(train_sentences_path), model_file=word_vectors_model_path)
+    test_model = Sent2Vec(LineSentence(test_sentences_path), model_file=word_vectors_model_path)
+
+# Save the vectors into files
+train_model.save_sent2vec_format(train_vectors_path)
+test_model.save_sent2vec_format(test_vectors_path)
+
+program = os.path.basename(sys.argv[0])
+logging.info("finished running %s" % program)
