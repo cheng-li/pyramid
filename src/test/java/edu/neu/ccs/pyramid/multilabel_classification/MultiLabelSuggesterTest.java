416eefb8844c1e34842968300527b22565dfe286
Cheng Li <chengli.email@gmail.com>
2015-09-28 14:23:24 -0400

    suggester test

--- src/test/java/edu/neu/ccs/pyramid/multilabel_classification/MultiLabelSuggesterTest.java
+++ src/test/java/edu/neu/ccs/pyramid/multilabel_classification/MultiLabelSuggesterTest.java
@@ -1,11 +1,14 @@
 package edu.neu.ccs.pyramid.multilabel_classification;
 
+import edu.neu.ccs.pyramid.classification.naive_bayes.Bernoulli;
 import edu.neu.ccs.pyramid.configuration.Config;
 import edu.neu.ccs.pyramid.dataset.*;
 import edu.neu.ccs.pyramid.util.SetUtil;
+import org.apache.commons.math3.distribution.BinomialDistribution;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -16,7 +19,7 @@ public class MultiLabelSuggesterTest {
     private static final String DATASETS = config.getString("input.datasets");
     private static final String TMP = config.getString("output.tmp");
     public static void main(String[] args) throws Exception{
-        test4();
+        test5();
     }
 
     private static void test1()throws Exception{
@@ -61,6 +64,25 @@ public class MultiLabelSuggesterTest {
             System.out.println(multiLabel+"\t"+newintest.contains(multiLabel));
         }
 
+
+    }
+
+    private static void test5(){
+        MultiLabelClfDataSet dataSet = MLClfDataSetBuilder.getBuilder()
+                .numDataPoints(100000).numFeatures(1)
+                .numClasses(5).build();
+        BinomialDistribution binomialDistribution  = new BinomialDistribution(1,0.25);
+        for (int i=0;i<dataSet.getNumDataPoints();i++){
+            for (int k=0;k<5;k++){
+
+                double sample = binomialDistribution.sample();
+                if (sample==1){
+                    dataSet.addLabel(i, k);
+                }
+            }
+        }
+        MultiLabelSuggester suggester = new MultiLabelSuggester(dataSet,32);
+        System.out.println(suggester.getBmm());
 
     }
 
