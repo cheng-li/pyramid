3942ab2f1c7bab6a8b22bbeebbe0155a01555d45
Cheng Li <chengli.email@gmail.com>
2015-10-07 19:13:22 -0400

    kl divergence

--- src/main/java/edu/neu/ccs/pyramid/eval/KLDivergence.java
+++ src/main/java/edu/neu/ccs/pyramid/eval/KLDivergence.java
@@ -1,7 +1,20 @@
 package edu.neu.ccs.pyramid.eval;
 
+import org.apache.commons.math3.util.FastMath;
+
 /**
  * Created by chengli on 11/11/14.
  */
 public class KLDivergence {
+    public static double kl(double[] trueDistribution, double[] estimatedDistribution){
+        double r = 0;
+        for (int i=0;i<trueDistribution.length;i++){
+            if (trueDistribution[i]==0){
+                r += 0;
+            } else {
+                r += trueDistribution[i]* FastMath.log(2, trueDistribution[i] / estimatedDistribution[i]);
+            }
+        }
+        return r;
+    }
 }
