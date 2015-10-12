7665268444c152eb30b0850f32b712b9710aefdb
Cheng Li <chengli.email@gmail.com>
2015-10-12 11:22:03 -0400

    more methods in KLDivergence

--- src/main/java/edu/neu/ccs/pyramid/eval/KLDivergence.java
+++ src/main/java/edu/neu/ccs/pyramid/eval/KLDivergence.java
@@ -1,18 +1,26 @@
 package edu.neu.ccs.pyramid.eval;
 
-import org.apache.commons.math3.util.FastMath;
-
 /**
  * Created by chengli on 11/11/14.
  */
 public class KLDivergence {
-    public static double kl(double[] trueDistribution, double[] estimatedDistribution){
+    public static double kl(double[] targetDistribution, double[] estimatedDistribution){
         double r = 0;
-        for (int i=0;i<trueDistribution.length;i++){
-            if (trueDistribution[i]==0){
-                r += 0;
-            } else {
-                r += trueDistribution[i]* Math.log(trueDistribution[i] / estimatedDistribution[i]);
+        for (int i=0;i<targetDistribution.length;i++){
+            // if ==0, don't change sum
+            if (targetDistribution[i]!=0){
+                r += targetDistribution[i]* Math.log(targetDistribution[i] / estimatedDistribution[i]);
+            }
+        }
+        return r;
+    }
+
+    public static double klGivenPLogQ(double[] targetDistribution, double[] logEstimatedDistribution){
+        double r = 0;
+        for (int i=0;i<targetDistribution.length;i++){
+            // if ==0, don't change sum
+            if (targetDistribution[i]!=0){
+                r += targetDistribution[i]* (Math.log(targetDistribution[i])-logEstimatedDistribution[i]);
             }
         }
         return r;
