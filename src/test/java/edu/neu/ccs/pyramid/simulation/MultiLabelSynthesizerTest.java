7cb9257b86dc7d852826a2c2b4cf1ebeb1fd54a4
Cheng Li <chengli.email@gmail.com>
2015-12-12 13:28:04 -0500

    more tests

--- src/test/java/edu/neu/ccs/pyramid/simulation/MultiLabelSynthesizerTest.java
+++ src/test/java/edu/neu/ccs/pyramid/simulation/MultiLabelSynthesizerTest.java
@@ -32,9 +32,13 @@ public class MultiLabelSynthesizerTest {
 //        test3_br();
 //        test3_mix();
 //        test4Dump();
-        test4_br();
-        test4_mix();
-        test4_powerset();
+//        test4_br();
+//        test4_powerset();
+//        test4_mix();
+//        test5Dump();
+//                test5_br();
+//        test5_powerset();
+        test5_mix();
     }
 
     private static void test1_br(){
@@ -226,9 +230,9 @@ public class MultiLabelSynthesizerTest {
 
 
     private static void test4Dump(){
-        MultiLabelClfDataSet all = MultiLabelSynthesizer.flipOne(1000,2,2);
-        List<Integer> trainIndices = IntStream.range(0,500).mapToObj(i -> i).collect(Collectors.toList());
-        List<Integer> testIndices = IntStream.range(500,1000).mapToObj(i->i).collect(Collectors.toList());
+        MultiLabelClfDataSet all = MultiLabelSynthesizer.flipOne(10000,2,3);
+        List<Integer> trainIndices = IntStream.range(0,5000).mapToObj(i -> i).collect(Collectors.toList());
+        List<Integer> testIndices = IntStream.range(5000,10000).mapToObj(i->i).collect(Collectors.toList());
         DataSetUtil.sampleData(all,trainIndices);
         MultiLabelClfDataSet trainSet = DataSetUtil.sampleData(all, trainIndices);
         MultiLabelClfDataSet testSet =  DataSetUtil.sampleData(all, testIndices);
@@ -275,9 +279,10 @@ public class MultiLabelSynthesizerTest {
         LPClassifier classifier = new LPClassifier(trainSet);
         LPOptimizer optimizer = new LPOptimizer(classifier,trainSet);
         Config config1 = new Config();
-        config1.setString("classifier","logistic");
+        config1.setString("classifier","lkboost");
         config1.setDouble("l1Ratio", 0);
         config1.setDouble("regularization", 0.00001);
+        config1.setInt("numIters",200);
         optimizer.optimize(config1);
 
         MultiLabel[] trainPredict;
@@ -298,6 +303,106 @@ public class MultiLabelSynthesizerTest {
         MultiLabelClfDataSet trainSet = TRECFormat.loadMultiLabelClfDataSet(new File(TMP, "train.trec"), DataSetType.ML_CLF_DENSE, true);
         MultiLabelClfDataSet testSet = TRECFormat.loadMultiLabelClfDataSet(new File(TMP, "test.trec"), DataSetType.ML_CLF_DENSE, true);
         int numClusters = 5;
+        double softmaxVariance = 100;
+        double logitVariance = 100;
+        BMMClassifier bmmClassifier = new BMMClassifier(trainSet.getNumClasses(),numClusters,trainSet.getNumFeatures());
+        BMMOptimizer optimizer = new BMMOptimizer(bmmClassifier, trainSet,softmaxVariance,logitVariance);
+        bmmClassifier.setAllowEmpty(true);
+        bmmClassifier.setPredictMode("dynamic");
+        BMMInitializer.initialize(bmmClassifier, trainSet, softmaxVariance, logitVariance);
+        for (int i=1;i<=20;i++){
+            optimizer.iterate();
+            MultiLabel[] trainPredict;
+            MultiLabel[] testPredict;
+            trainPredict = bmmClassifier.predict(trainSet);
+            testPredict = bmmClassifier.predict(testSet);
+            System.out.print("iter : "+i + "\t");
+            System.out.print("objective: "+optimizer.getTerminator().getLastValue() + "\t");
+            System.out.print("train Hamming loss : " + HammingLoss.hammingLoss(bmmClassifier, trainSet) + "\t");
+            System.out.print("trainAcc : "+ Accuracy.accuracy(trainSet.getMultiLabels(), trainPredict)+ "\t");
+            System.out.print("trainOver: "+ Overlap.overlap(trainSet.getMultiLabels(), trainPredict)+ "\t");
+            System.out.print("test Hamming loss : " + HammingLoss.hammingLoss(bmmClassifier, testSet) + "\t");
+            System.out.print("testAcc  : " + Accuracy.accuracy(testSet.getMultiLabels(), testPredict) + "\t");
+            System.out.println("testOver : "+ Overlap.overlap(testSet.getMultiLabels(), testPredict)+ "\t");
+
+        }
+    }
+
+
+    private static void test5Dump() {
+        MultiLabelClfDataSet all = MultiLabelSynthesizer.flipTwo(10000,2,5);
+        List<Integer> trainIndices = IntStream.range(0,5000).mapToObj(i -> i).collect(Collectors.toList());
+        List<Integer> testIndices = IntStream.range(5000,10000).mapToObj(i->i).collect(Collectors.toList());
+        DataSetUtil.sampleData(all,trainIndices);
+        MultiLabelClfDataSet trainSet = DataSetUtil.sampleData(all, trainIndices);
+        MultiLabelClfDataSet testSet =  DataSetUtil.sampleData(all, testIndices);
+        TRECFormat.save(trainSet, new File(TMP,"train.trec"));
+        TRECFormat.save(testSet, new File(TMP,"test.trec"));
+    }
+
+
+    private static void test5_br() throws Exception{
+        System.out.println("binary");
+        MultiLabelClfDataSet trainSet = TRECFormat.loadMultiLabelClfDataSet(new File(TMP,"train.trec"), DataSetType.ML_CLF_DENSE,true);
+        MultiLabelClfDataSet testSet = TRECFormat.loadMultiLabelClfDataSet(new File(TMP, "test.trec"), DataSetType.ML_CLF_DENSE, true);
+        int numClusters = 1;
+        double softmaxVariance = 100;
+        double logitVariance = 100;
+        BMMClassifier bmmClassifier = new BMMClassifier(trainSet.getNumClasses(),numClusters,trainSet.getNumFeatures());
+        BMMOptimizer optimizer = new BMMOptimizer(bmmClassifier, trainSet,softmaxVariance,logitVariance);
+        bmmClassifier.setAllowEmpty(true);
+        bmmClassifier.setPredictMode("dynamic");
+        BMMInitializer.initialize(bmmClassifier, trainSet, softmaxVariance, logitVariance);
+        for (int i=1;i<=1;i++){
+            optimizer.iterate();
+            MultiLabel[] trainPredict;
+            MultiLabel[] testPredict;
+            trainPredict = bmmClassifier.predict(trainSet);
+            testPredict = bmmClassifier.predict(testSet);
+            System.out.print("iter : "+i + "\t");
+            System.out.print("objective: "+optimizer.getTerminator().getLastValue() + "\t");
+            System.out.print("train Hamming loss : " + HammingLoss.hammingLoss(bmmClassifier, trainSet) + "\t");
+            System.out.print("trainAcc : "+ Accuracy.accuracy(trainSet.getMultiLabels(), trainPredict)+ "\t");
+            System.out.print("trainOver: "+ Overlap.overlap(trainSet.getMultiLabels(), trainPredict)+ "\t");
+            System.out.print("test Hamming loss : " + HammingLoss.hammingLoss(bmmClassifier, testSet) + "\t");
+            System.out.print("testAcc  : " + Accuracy.accuracy(testSet.getMultiLabels(), testPredict) + "\t");
+            System.out.print("testOver : " + Overlap.overlap(testSet.getMultiLabels(), testPredict) + "\t");
+        }
+    }
+
+
+    private static void test5_powerset() throws Exception{
+        System.out.println("powerset");
+        MultiLabelClfDataSet trainSet = TRECFormat.loadMultiLabelClfDataSet(new File(TMP, "train.trec"), DataSetType.ML_CLF_DENSE, true);
+        MultiLabelClfDataSet testSet = TRECFormat.loadMultiLabelClfDataSet(new File(TMP, "test.trec"), DataSetType.ML_CLF_DENSE, true);
+
+        LPClassifier classifier = new LPClassifier(trainSet);
+        LPOptimizer optimizer = new LPOptimizer(classifier,trainSet);
+        Config config1 = new Config();
+        config1.setString("classifier","logistic");
+        config1.setDouble("l1Ratio", 0);
+        config1.setDouble("regularization", 0.00001);
+        config1.setInt("numIters",200);
+        optimizer.optimize(config1);
+
+        MultiLabel[] trainPredict;
+        MultiLabel[] testPredict;
+        trainPredict = classifier.predict(trainSet);
+        testPredict = classifier.predict(testSet);
+        System.out.print("train Hamming loss : " + HammingLoss.hammingLoss(classifier, trainSet) + "\t");
+        System.out.print("trainAcc : "+ Accuracy.accuracy(trainSet.getMultiLabels(), trainPredict)+ "\t");
+        System.out.print("trainOver: "+ Overlap.overlap(trainSet.getMultiLabels(), trainPredict)+ "\t");
+        System.out.print("test Hamming loss : " + HammingLoss.hammingLoss(classifier, testSet) + "\t");
+        System.out.print("testAcc  : " + Accuracy.accuracy(testSet.getMultiLabels(), testPredict) + "\t");
+        System.out.println("testOver : "+ Overlap.overlap(testSet.getMultiLabels(), testPredict)+ "\t");
+    }
+
+
+    private static void test5_mix() throws Exception{
+        System.out.println("mix");
+        MultiLabelClfDataSet trainSet = TRECFormat.loadMultiLabelClfDataSet(new File(TMP, "train.trec"), DataSetType.ML_CLF_DENSE, true);
+        MultiLabelClfDataSet testSet = TRECFormat.loadMultiLabelClfDataSet(new File(TMP, "test.trec"), DataSetType.ML_CLF_DENSE, true);
+        int numClusters = 20;
         double softmaxVariance = 100;
         double logitVariance = 100;
         BMMClassifier bmmClassifier = new BMMClassifier(trainSet.getNumClasses(),numClusters,trainSet.getNumFeatures());
