5954232512a8d5ad0dde3667a9f5a5284be7353e
Cheng Li <chengli.email@gmail.com>
2016-01-25 14:18:09 -0500

    update exp143

--- src/main/java/edu/neu/ccs/pyramid/experiment/Exp143.java
+++ src/main/java/edu/neu/ccs/pyramid/experiment/Exp143.java
@@ -13,8 +13,36 @@ import java.util.List;
 public class Exp143 {
     public static void main(String[] args) throws Exception{
         String[][] table = load();
-        System.out.println(Arrays.deepToString(table));
+        System.out.println(table.length);
+        System.out.println(table[0].length);
+//        dataTable(table);
+        performance(table);
 
+    }
+
+    static void performance(String[][] table){
+        int numData=5;
+        int numAlgorithms=8;
+        String[] dataNames = {"scene","emotions","mediamill","NUSWIDE","TMC2007"};
+        int[] dataRows = {1,5,7,13,17};
+        String[] algorithms = {"CBM+LR","CBM+Boost","CRF","BR+LR","BR+Boost","PS+LR","PS+Boost","CC+LR"};
+        int[] algoC = {9,3,11,13,15,16,17,18};
+        StringBuilder sb = new StringBuilder();
+        for (int i=0;i<numAlgorithms;i++){
+            sb.append(algorithms[i]).append(" ");
+            for (int j=0;j<numData;j++){
+                System.out.println("i="+i+"j="+j);
+                System.out.println(""+dataRows[j]+","+algoC[i]);
+                sb.append("&").append(table[dataRows[j]][algoC[i]]);
+                sb.append("&").append(table[dataRows[j]+1][algoC[i]]);
+            }
+            sb.append("\\\\").append("\n").append("\\hline").append("\n");
+        }
+        System.out.println(sb.toString());
+
+    }
+
+    static void dataTable(String[][] table){
         int numData=5;
         String[] dataNames = {"scene","emotions","mediamill","NUSWIDE","TMC2007"};
         int[] dataRows = {1,5,7,13,17};
