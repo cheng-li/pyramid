7cd8fb239aa12a48d9433a6735806b68f4721192
Cheng Li <chengli.email@gmail.com>
2016-02-20 17:46:38 -0500

    Move ECOC, SVM, NB to private repo

--- src/main/java/edu/neu/ccs/pyramid/classification/naive_bayes/DistributionType.java
+++
@@ -1,8 +0,0 @@
-package edu.neu.ccs.pyramid.classification.naive_bayes;
-
-/**
- * Created by Rainicy on 10/8/14.
- */
-public enum DistributionType {
-    HISTOGRAM, GAUSSIAN, GAMMA
-}
--- src/main/java/edu/neu/ccs/pyramid/classification/svm/Kernel.java
+++
@@ -1,7 +0,0 @@
-package edu.neu.ccs.pyramid.classification.svm;
-
-/**
- * Created by Rainicy on 11/27/14.
- */
-public class Kernel {
-}
