e627656009e6b22a007c047d62870d1a2bcca48a
Cheng Li <chengli.email@gmail.com>
2016-09-18 22:52:54 -0400

    reduce memory usage

--- src/main/java/edu/neu/ccs/pyramid/dataset/GradientMatrix.java
+++ src/main/java/edu/neu/ccs/pyramid/dataset/GradientMatrix.java
@@ -6,26 +6,20 @@ package edu.neu.ccs.pyramid.dataset;
 public class GradientMatrix {
     private int numDataPoints;
     private int numClasses;
-    private double[][] dataClass;
     private double[][] classData;
     private Objective objective;
 
     public GradientMatrix(int numDataPoints, int numClasses, Objective objective) {
         this.numDataPoints = numDataPoints;
         this.numClasses = numClasses;
-        this.dataClass = new double[numDataPoints][numClasses];
         this.classData = new double[numClasses][numDataPoints];
         this.objective = objective;
     }
 
     public void setGradient(int dataPointIndex, int classIndex, double gradient){
-        this.dataClass[dataPointIndex][classIndex] = gradient;
         this.classData[classIndex][dataPointIndex] = gradient;
     }
 
-    public double[] getGradientsForData(int dataPointIndex){
-        return dataClass[dataPointIndex];
-    }
 
     public double[] getGradientsForClass(int classIndex){
         return classData[classIndex];
