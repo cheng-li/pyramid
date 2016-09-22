0019011dcf1df4c0e64eb16b9e961766f17266e4
Cheng Li <chengli.email@gmail.com>
2016-09-22 16:23:06 -0400

    AppLSBoost properties

--- src/main/java/edu/neu/ccs/pyramid/application/AppLSBoost.java
+++ src/main/java/edu/neu/ccs/pyramid/application/AppLSBoost.java
@@ -38,7 +38,7 @@ public class AppLSBoost {
     }
 
     private static void train(Config config) throws Exception{
-        String sparsity = config.getString("input.matrixSparsity");
+        String sparsity = config.getString("input.matrixType");
         DataSetType dataSetType = null;
         switch (sparsity){
             case "dense":
@@ -48,13 +48,13 @@ public class AppLSBoost {
                 dataSetType = DataSetType.REG_SPARSE;
                 break;
             default:
-                throw new IllegalArgumentException("input.matrixSparsity should be dense or sparse");
+                throw new IllegalArgumentException("input.matrixType should be dense or sparse");
         }
         RegDataSet trainSet = TRECFormat.loadRegDataSet(config.getString("input.trainData"),dataSetType, true);
 
         RegDataSet testSet = null;
         if (config.getBoolean("train.showTestProgress")){
-            testSet = TRECFormat.loadRegDataSet(config.getString("input.trainData"),dataSetType, true);
+            testSet = TRECFormat.loadRegDataSet(config.getString("input.testData"),dataSetType, true);
         }
 
         LSBoost lsBoost = new LSBoost();
@@ -68,13 +68,13 @@ public class AppLSBoost {
         int progressInterval = config.getInt("train.showProgress.interval");
 
         int numIterations=config.getInt("train.numIterations");
-        for (int i=0;i<numIterations;i++){
+        for (int i=1;i<=numIterations;i++){
             System.out.println("iteration "+i);
             optimizer.iterate();
-            if (config.getBoolean("train.showTrainProgress") && (i%progressInterval==0 || i==numIterations-1)){
+            if (config.getBoolean("train.showTrainProgress") && (i%progressInterval==0 || i==numIterations)){
                 System.out.println("train RMSE = "+ RMSE.rmse(lsBoost, trainSet));
             }
-            if (config.getBoolean("train.showTestProgress") && (i%progressInterval==0 || i==numIterations-1)){
+            if (config.getBoolean("train.showTestProgress") && (i%progressInterval==0 || i==numIterations)){
                 System.out.println("test RMSE = "+ RMSE.rmse(lsBoost, testSet));
             }
         }
@@ -84,16 +84,16 @@ public class AppLSBoost {
         File serializedModel =  new File(output,"model");
         Serialization.serialize(lsBoost, serializedModel);
         System.out.println("model saved to "+serializedModel.getAbsolutePath());
-        File reportFile = new File(output, "train_report.txt");
+        File reportFile = new File(output, "train_predictions.txt");
         report(lsBoost, trainSet, reportFile);
-        System.out.println("training report written to "+reportFile.getAbsolutePath());
+        System.out.println("predictions on the training set are written to "+reportFile.getAbsolutePath());
     }
 
     private static void test(Config config) throws Exception{
         String output = config.getString("output.folder");
         File serializedModel =  new File(output,"model");
         LSBoost lsBoost = (LSBoost)Serialization.deserialize(serializedModel);
-        String sparsity = config.getString("input.matrixSparsity");
+        String sparsity = config.getString("input.matrixType");
         DataSetType dataSetType = null;
         switch (sparsity){
             case "dense":
@@ -103,13 +103,13 @@ public class AppLSBoost {
                 dataSetType = DataSetType.REG_SPARSE;
                 break;
             default:
-                throw new IllegalArgumentException("input.matrixSparsity should be dense or sparse");
+                throw new IllegalArgumentException("input.matrixType should be dense or sparse");
         }
-        RegDataSet testSet = TRECFormat.loadRegDataSet(config.getString("input.trainData"),dataSetType, true);
+        RegDataSet testSet = TRECFormat.loadRegDataSet(config.getString("input.testData"),dataSetType, true);
         System.out.println("test RMSE = "+ RMSE.rmse(lsBoost, testSet));
-        File reportFile = new File(output, "test_report.txt");
+        File reportFile = new File(output, "test_predictions.txt");
         report(lsBoost, testSet, reportFile);
-        System.out.println("testing report written to "+reportFile.getAbsolutePath());
+        System.out.println("predictions on the test set are written to "+reportFile.getAbsolutePath());
     }
 
     private static void report(LSBoost lsBoost, RegDataSet dataSet, File reportFile) throws IOException {
