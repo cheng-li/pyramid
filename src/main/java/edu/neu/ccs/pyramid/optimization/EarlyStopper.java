8c2f190ebff6d44f541cb0a020c1832ce01c99ed
Cheng Li <chengli.email@gmail.com>
2016-10-29 20:21:00 -0400

    add EarlyStopper

---
+++ src/main/java/edu/neu/ccs/pyramid/optimization/EarlyStopper.java
@@ -0,0 +1,91 @@
+package edu.neu.ccs.pyramid.optimization;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by chengli on 10/29/16.
+ */
+public class EarlyStopper {
+    private List<Double> values;
+    private List<Integer> iterations;
+    private double bestValue;
+    private int bestIteration = -1;
+    private Goal goal;
+    private int patience = 5;
+    private int patienceUses = 0;
+    private boolean shouldStop = false;
+
+    public EarlyStopper(Goal goal, int patience) {
+        this.goal = goal;
+        this.patience = patience;
+        if (goal==Goal.MAXIMIZE){
+            bestValue = Double.NEGATIVE_INFINITY;
+        } else {
+            bestValue = Double.POSITIVE_INFINITY;
+        }
+        this.values = new ArrayList<>();
+        this.iterations = new ArrayList<>();
+    }
+
+
+
+    public void add(int iteration, double value){
+
+        if (!shouldStop){
+            values.add(value);
+            iterations.add(iteration);
+            boolean better = isBetter(value);
+            if (better){
+                updateWithProgress(iteration, value);
+            } else {
+                updateWithoutProgress();
+            }
+        }
+    }
+
+    public int getBestIteration() {
+        return bestIteration;
+    }
+
+    public boolean shouldStop() {
+        return shouldStop;
+    }
+
+    public static enum Goal{
+        MAXIMIZE, MINIMIZE
+    }
+
+
+    private boolean isBetter(double value){
+        if (goal==Goal.MAXIMIZE && value > bestValue){
+            return true;
+        }
+
+        if (goal==Goal.MINIMIZE && value < bestValue){
+            return true;
+        }
+
+        return false;
+    }
+
+
+    private void updateWithProgress(int iteration, double value){
+        bestIteration = iteration;
+        bestValue = value;
+        patienceUses = 0;
+    }
+
+    private void updateWithoutProgress(){
+        patienceUses += 1;
+        if (patienceUses>=patience){
+            shouldStop = true;
+        }
+    }
+
+
+
+
+
+
+}
---
+++ src/test/java/edu/neu/ccs/pyramid/optimization/EarlyStopperTest.java
@@ -0,0 +1,46 @@
+package edu.neu.ccs.pyramid.optimization;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by chengli on 10/29/16.
+ */
+public class EarlyStopperTest {
+
+    public static void main(String[] args) {
+        test3();
+    }
+
+
+    private static void test1(){
+        EarlyStopper earlyStopper = new EarlyStopper(EarlyStopper.Goal.MAXIMIZE, 2);
+        double[] values = {0.5, 0.6, 0.5, 0.5, 0.7};
+        for (int i=0;i<values.length;i++){
+            earlyStopper.add(i, values[i]);
+            System.out.println(earlyStopper.shouldStop());
+            System.out.println(earlyStopper.getBestIteration());
+        }
+    }
+
+    private static void test2(){
+        EarlyStopper earlyStopper = new EarlyStopper(EarlyStopper.Goal.MAXIMIZE, 2);
+        double[] values = {0.5, 0.6, 0.5, 0.7, 0.5, 0.4, 0.8};
+        for (int i=0;i<values.length;i++){
+            earlyStopper.add(i, values[i]);
+            System.out.println(earlyStopper.shouldStop());
+            System.out.println(earlyStopper.getBestIteration());
+        }
+    }
+
+
+    private static void test3(){
+        EarlyStopper earlyStopper = new EarlyStopper(EarlyStopper.Goal.MINIMIZE, 2);
+        double[] values = {0.5, 0.6, 0.4, 0.7, 0.5, 0.4, 0.8};
+        for (int i=0;i<values.length;i++){
+            earlyStopper.add(i, values[i]);
+            System.out.println(earlyStopper.shouldStop());
+            System.out.println(earlyStopper.getBestIteration());
+        }
+    }
+
+}
