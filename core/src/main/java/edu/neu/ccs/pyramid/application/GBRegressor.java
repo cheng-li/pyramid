84d15cffbace7a93b7cac44107ee58ad6eb3a24b
Zhenming Bi <zhenmingbi@gmail.com>
2017-12-14 22:16:28 -0500

    add logger add option to skip PMML

--- core/src/main/java/edu/neu/ccs/pyramid/application/GBRegressor.java
+++ core/src/main/java/edu/neu/ccs/pyramid/application/GBRegressor.java
@@ -16,6 +16,9 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 /**
  * least square gradient boosting for regression
@@ -27,31 +30,66 @@ public class GBRegressor {
             throw new IllegalArgumentException("Please specify a properties file.");
         }
         Config config = new Config(args[0]);
-        System.out.println(config);
+
+        Logger logger = Logger.getAnonymousLogger();
+        String logFile = config.getString("output.log");
+        FileHandler fileHandler = null;
+        if (!logFile.isEmpty()){
+            new File(logFile).getParentFile().mkdirs();
+            //todo should append?
+            fileHandler = new FileHandler(logFile, true);
+            java.util.logging.Formatter formatter = new SimpleFormatter();
+            fileHandler.setFormatter(formatter);
+            logger.addHandler(fileHandler);
+            logger.setUseParentHandlers(false);
+        }
+
+        logger.info(config.toString());
 
         if (config.getBoolean("train")){
-            train(config);
+            train(config,logger);
         }
 
         if (config.getBoolean("test")){
-            test(config);
+            test(config,logger);
+        }
+
+        if (fileHandler!=null){
+            fileHandler.close();
         }
     }
 
     public static void main(Config config) throws Exception{
 
-        System.out.println(config);
+        Logger logger = Logger.getAnonymousLogger();
+        String logFile = config.getString("output.log");
+        FileHandler fileHandler = null;
+        if (!logFile.isEmpty()){
+            new File(logFile).getParentFile().mkdirs();
+            //todo should append?
+            fileHandler = new FileHandler(logFile, true);
+            java.util.logging.Formatter formatter = new SimpleFormatter();
+            fileHandler.setFormatter(formatter);
+            logger.addHandler(fileHandler);
+            logger.setUseParentHandlers(false);
+        }
+
+        logger.info(config.toString());
 
         if (config.getBoolean("train")){
-            train(config);
+            train(config,logger);
         }
 
         if (config.getBoolean("test")){
-            test(config);
+            test(config,logger);
+        }
+
+        if (fileHandler!=null){
+            fileHandler.close();
         }
     }
 
-    private static void train(Config config) throws Exception{
+    private static void train(Config config, Logger logger) throws Exception{
         String sparsity = config.getString("input.matrixType");
         DataSetType dataSetType = null;
         switch (sparsity){
@@ -83,32 +121,35 @@ public class GBRegressor {
 
         int numIterations=config.getInt("train.numIterations");
         for (int i=1;i<=numIterations;i++){
-            System.out.println("iteration "+i);
+            logger.info("iteration "+i);
             optimizer.iterate();
             if (config.getBoolean("train.showTrainProgress") && (i%progressInterval==0 || i==numIterations)){
-                System.out.println("training RMSE = "+ RMSE.rmse(lsBoost, trainSet));
+                logger.info("training RMSE = "+ RMSE.rmse(lsBoost, trainSet));
             }
             if (config.getBoolean("train.showTestProgress") && (i%progressInterval==0 || i==numIterations)){
-                System.out.println("test RMSE = "+ RMSE.rmse(lsBoost, testSet));
+                logger.info("test RMSE = "+ RMSE.rmse(lsBoost, testSet));
             }
         }
-        System.out.println("training done!");
+        logger.info("training done!");
         String output = config.getString("output.folder");
         new File(output).mkdirs();
         File serializedModel =  new File(output,"model");
         Serialization.serialize(lsBoost, serializedModel);
-        System.out.println("model saved to "+serializedModel.getAbsolutePath());
+        logger.info("model saved to "+serializedModel.getAbsolutePath());
 
-        File pmmlModel = new File(output, "model.pmml");
-        PMMLConverter.savePMML(lsBoost,pmmlModel);
-        System.out.println("PMML model saved to "+pmmlModel.getAbsolutePath());
+        if(config.getBoolean("output.generatePMML")){
+            File pmmlModel = new File(output, "model.pmml");
+            PMMLConverter.savePMML(lsBoost,pmmlModel);
+            logger.info("PMML model saved to "+pmmlModel.getAbsolutePath());
+
+        }
 
         File reportFile = new File(output, "train_predictions.txt");
         report(lsBoost, trainSet, reportFile);
-        System.out.println("predictions on the training set are written to "+reportFile.getAbsolutePath());
+        logger.info("predictions on the training set are written to "+reportFile.getAbsolutePath());
     }
 
-    private static void test(Config config) throws Exception{
+    private static void test(Config config, Logger logger) throws Exception{
         String output = config.getString("output.folder");
         File serializedModel =  new File(output,"model");
         LSBoost lsBoost = (LSBoost)Serialization.deserialize(serializedModel);
@@ -125,10 +166,10 @@ public class GBRegressor {
                 throw new IllegalArgumentException("input.matrixType should be dense or sparse");
         }
         RegDataSet testSet = TRECFormat.loadRegDataSet(config.getString("input.testData"),dataSetType, true);
-        System.out.println("test RMSE = "+ RMSE.rmse(lsBoost, testSet));
+        logger.info("test RMSE = "+ RMSE.rmse(lsBoost, testSet));
         File reportFile = new File(output, "test_predictions.txt");
         report(lsBoost, testSet, reportFile);
-        System.out.println("predictions on the test set are written to "+reportFile.getAbsolutePath());
+        logger.info("predictions on the test set are written to "+reportFile.getAbsolutePath());
     }
 
     private static void report(LSBoost lsBoost, RegDataSet dataSet, File reportFile) throws IOException {
