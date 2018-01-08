707259033c97f1c00fd7c85ecb74e136f464e2b1
Cheng Li <chengli.email@gmail.com>
2018-01-08 16:30:17 -0500

    gmm test

--- core/src/main/java/edu/neu/ccs/pyramid/clustering/gmm/GMM.java
+++ core/src/main/java/edu/neu/ccs/pyramid/clustering/gmm/GMM.java
@@ -1,12 +1,39 @@
 package edu.neu.ccs.pyramid.clustering.gmm;
 
 import edu.neu.ccs.pyramid.util.MathUtil;
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
+
+import java.util.Arrays;
 
 public class GMM {
     private int numComponents;
     private GaussianDistribution[] gaussianDistributions;
     private double[] mixtureCoefficients;
+
+    public GMM(int dimension, int numComponents) {
+        this.numComponents = numComponents;
+        this.mixtureCoefficients = new double[numComponents];
+        Arrays.fill(this.mixtureCoefficients,1.0/numComponents);
+        this.gaussianDistributions = new GaussianDistribution[numComponents];
+        for (int k=0;k<numComponents;k++){
+            RealVector mean = new ArrayRealVector(dimension);
+            for (int d=0;d<dimension;d++){
+                mean.setEntry(d,Math.random());
+            }
+            RealMatrix cov = new Array2DRowRealMatrix(dimension,dimension);
+            for (int d=0;d<dimension;d++){
+                cov.setEntry(d,d,1);
+            }
+            gaussianDistributions[k] = new GaussianDistribution(mean, cov);
+        }
+    }
+
+    public void setMixtureCoefficient(int k, double value){
+        mixtureCoefficients[k] = value;
+    }
 
     public int getNumComponents() {
         return numComponents;
@@ -41,5 +68,13 @@ public class GMM {
         return posteriors;
     }
 
-
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("GMM{");
+        sb.append("numComponents=").append(numComponents);
+        sb.append(", gaussianDistributions=").append(Arrays.toString(gaussianDistributions));
+        sb.append(", mixtureCoefficients=").append(Arrays.toString(mixtureCoefficients));
+        sb.append('}');
+        return sb.toString();
+    }
 }
---
+++ core/src/test/java/edu/neu/ccs/pyramid/clustering/gmm/GMMTrainerTest.java
@@ -0,0 +1,37 @@
+package edu.neu.ccs.pyramid.clustering.gmm;
+
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.RealMatrix;
+
+import java.io.File;
+import java.util.List;
+
+public class GMMTrainerTest {
+    public static void main(String[] args) throws Exception{
+        List<String> lines = FileUtils.readLines(new File("/Users/chengli/tmp/3gaussian.txt"));
+        int dim = lines.get(0).split(" ").length;
+        RealMatrix data = new Array2DRowRealMatrix(lines.size(),dim);
+        for (int i=0;i<lines.size();i++){
+            String[] split = lines.get(i).split(" ");
+            for (int j=0;j<dim;j++){
+                data.setEntry(i,j,Double.parseDouble(split[j]));
+            }
+        }
+
+
+        GMM gmm = new GMM(dim,3);
+
+        GMMTrainer trainer = new GMMTrainer(data, gmm);
+
+        for (int i=0;i<1000;i++){
+            trainer.iterate();
+        }
+
+        System.out.println(gmm);
+    }
+
+
+
+}
