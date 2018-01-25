e3b3755cec2f4216409e06fe3e5cb264fad40d02
Zhenming Bi <zhenmingbi@gmail.com>
2018-01-24 19:15:36 -0500

    code description to json

--- core/src/main/java/edu/neu/ccs/pyramid/feature/CodeDescription.java
+++ core/src/main/java/edu/neu/ccs/pyramid/feature/CodeDescription.java
@@ -1,10 +1,18 @@
 package edu.neu.ccs.pyramid.feature;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.io.IOException;
 import java.util.List;
 
 /**
  * Created by chengli on 10/6/16.
  */
+@JsonSerialize(using = CodeDescription.Serializer.class)
 public class CodeDescription extends Feature {
     private static final long serialVersionUID = 1L;
     private List<String> description;
@@ -37,5 +45,23 @@ public class CodeDescription extends Feature {
         sb.append(", field='").append(field).append('\'');
         sb.append('}');
         return sb.toString();
+    }
+
+    public static class Serializer extends JsonSerializer<CodeDescription> {
+        @Override
+        public void serialize(CodeDescription feature, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException, JsonProcessingException {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (String term: feature.description){
+                stringBuilder.append(term).append(" ");
+            }
+            jsonGenerator.writeStartObject();
+            jsonGenerator.writeNumberField("index", feature.index);
+            jsonGenerator.writeStringField("type","code description");
+            jsonGenerator.writeStringField("field",feature.field);
+            jsonGenerator.writeStringField("description",stringBuilder.toString());
+            jsonGenerator.writeNumberField("minimum should match percent",feature.percentage);
+
+            jsonGenerator.writeEndObject();
+        }
     }
 }
