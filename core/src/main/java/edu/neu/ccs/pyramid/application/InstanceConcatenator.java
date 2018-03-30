00a2e932d75d22fc287331d75f3975b3773f6f6d
Cheng Li <chengli.email@gmail.com>
2018-03-29 23:00:12 -0400

    InstanceConcatenator

---
+++ core/src/main/java/edu/neu/ccs/pyramid/application/InstanceConcatenator.java
@@ -0,0 +1,35 @@
+package edu.neu.ccs.pyramid.application;
+
+import edu.neu.ccs.pyramid.configuration.Config;
+import edu.neu.ccs.pyramid.dataset.DataSetType;
+import edu.neu.ccs.pyramid.dataset.DataSetUtil;
+import edu.neu.ccs.pyramid.dataset.MultiLabelClfDataSet;
+import edu.neu.ccs.pyramid.dataset.TRECFormat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InstanceConcatenator {
+
+    public static void main(String[] args) throws Exception{
+        if (args.length != 1) {
+            throw new IllegalArgumentException("Please specify a properties file.");
+        }
+
+        Config config = new Config(args[0]);
+
+        List<String> inputs = config.getStrings("input.dataSets");
+        String output = config.getString("output.dataSet");
+
+        List<MultiLabelClfDataSet> dataSets = new ArrayList<>();
+        for (String input: inputs){
+            //todo
+//            MultiLabelClfDataSet dataSet = TRECFormat.loadMultiLabelClfDataSet(input, DataSetType.ML_CLF_SPARSE,true);
+            MultiLabelClfDataSet dataSet = TRECFormat.loadMultiLabelClfDataSet(input, DataSetType.ML_CLF_DENSE,true);
+            dataSets.add(dataSet);
+        }
+
+        MultiLabelClfDataSet merged = DataSetUtil.concatenateByRow(dataSets);
+        TRECFormat.save(merged,output);
+    }
+}
