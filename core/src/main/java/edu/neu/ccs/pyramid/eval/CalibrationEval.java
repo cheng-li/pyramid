dfcdb4d182eef58d870f347bc447df6a0706a323
Cheng Li <chengli.email@gmail.com>
2018-04-09 22:15:12 -0400

    fix stream issue

--- core/src/main/java/edu/neu/ccs/pyramid/eval/CalibrationEval.java
+++ core/src/main/java/edu/neu/ccs/pyramid/eval/CalibrationEval.java
@@ -18,11 +18,15 @@ public class CalibrationEval {
             double count = bucketInfo.getCounts()[i];
             sum += count*Math.abs(bucketInfo.getSumProbs()[i]/count-bucketInfo.getSums()[i]/count);
         }
-        return sum/stream.count();
+        int total = 0;
+        for (int i=0;i<bucketInfo.getNumBuckets();i++){
+            total += bucketInfo.getCounts()[i];
+        }
+        return sum/total;
     }
 
     public static double sharpness(Stream<Pair<Double, Integer>> stream, int numBuckets){
-        long total = stream.count();
+
         BucketInfo bucketInfo = BucketInfo.aggregate(stream, numBuckets);
         double[] accuracies = new double[bucketInfo.getNumBuckets()];
         for (int i=0;i<bucketInfo.getNumBuckets();i++){
@@ -31,6 +35,11 @@ public class CalibrationEval {
         double average = 0;
         for (int i=0;i<bucketInfo.getNumBuckets();i++){
             average += bucketInfo.getSums()[i];
+        }
+
+        int total = 0;
+        for (int i=0;i<bucketInfo.getNumBuckets();i++){
+            total += bucketInfo.getCounts()[i];
         }
 
         average/=total;
@@ -43,12 +52,35 @@ public class CalibrationEval {
     }
 
     public static double variance(Stream<Pair<Double, Integer>> stream){
-        long count = stream.count();
-        double sum = stream.mapToDouble(pair->pair.getSecond()).sum();
-        double sumSquare = stream.mapToDouble(pair->Math.pow(pair.getSecond(),2)).sum();
+        VarResult varResult = stream.collect(()->new VarResult(),VarResult::add, VarResult::add);
+        double count = varResult.count;
+        double sum = varResult.sum;
+        double sumSquare = varResult.sumSquare;
         return (sumSquare-sum*sum/count)/(count-1);
 
     }
 
 
+
+    public static class VarResult{
+        double sum;
+        double sumSquare;
+        double count;
+
+        public VarResult() {
+        }
+
+         void add(VarResult varResult){
+            this.sum += varResult.sum;
+            this.count += varResult.count;
+            this.sumSquare += varResult.sumSquare;
+        }
+
+         void add(Pair<Double, Integer> pair){
+            this.sum += pair.getSecond();
+            this.count += 1;
+            this.sumSquare += Math.pow(pair.getSecond(),2);
+        }
+
+    }
 }
