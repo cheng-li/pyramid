49e0e45e373e9bd14a658a732283da21650a9ea7
Cheng Li <chengli.email@gmail.com>
2018-04-11 18:30:26 -0400

    generate stream

--- core/src/main/java/edu/neu/ccs/pyramid/calibration/StreamGenerator.java
+++ core/src/main/java/edu/neu/ccs/pyramid/calibration/StreamGenerator.java
@@ -1,11 +1,22 @@
 package edu.neu.ccs.pyramid.calibration;
 
 import edu.neu.ccs.pyramid.configuration.Config;
+import edu.neu.ccs.pyramid.dataset.MultiLabel;
 import edu.neu.ccs.pyramid.dataset.MultiLabelClfDataSet;
+import edu.neu.ccs.pyramid.multilabel_classification.imlgb.HammingPredictor;
+import edu.neu.ccs.pyramid.multilabel_classification.imlgb.IMLGBLabelIsotonicScaling;
 import edu.neu.ccs.pyramid.multilabel_classification.imlgb.IMLGradientBoosting;
+import edu.neu.ccs.pyramid.multilabel_classification.imlgb.SubsetAccPredictor;
+import edu.neu.ccs.pyramid.util.MathUtil;
 import edu.neu.ccs.pyramid.util.Pair;
+import org.apache.mahout.math.DenseVector;
 import org.apache.mahout.math.Vector;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamGenerator {
@@ -13,8 +24,87 @@ public class StreamGenerator {
     private Config config;
     private IMLGradientBoosting boosting;
     private MultiLabelClfDataSet dataSet;
+    private IMLGBLabelIsotonicScaling labelCali;
+
+    public StreamGenerator(Config config, IMLGradientBoosting boosting,
+                           MultiLabelClfDataSet dataSet, IMLGBLabelIsotonicScaling labelCali) {
+        this.config = config;
+        this.boosting = boosting;
+        this.dataSet = dataSet;
+        this.labelCali = labelCali;
+    }
 
     public  Stream<Pair<Vector,Integer>> generateStream(){
-        return null;
+        return IntStream.range(0, dataSet.getNumDataPoints())
+                .boxed().flatMap(this::generateStream);
+    }
+
+    public Stream<Pair<Vector,Integer>> generateStream(int data){
+        double[] uncalibratedLabelProbs = boosting.predictClassProbs(dataSet.getRow(data));
+        double[] calibratedLabelProbs = labelCali.calibratedClassProbs(uncalibratedLabelProbs);
+
+        MultiLabel top;
+        if (config.getString("A").equals("1")){
+            HammingPredictor predictor = new HammingPredictor(boosting);
+            top = predictor.predict(dataSet.getRow(data));
+        } else {
+            SubsetAccPredictor predictor = new SubsetAccPredictor(boosting);
+            top = predictor.predict(dataSet.getRow(data));
+        }
+
+        Set<MultiLabel> candidateSet = new HashSet<>();
+        candidateSet.add(top);
+        if (config.getString("D").equals("2")){
+            candidateSet.addAll(boosting.getAssignments());
+        }
+        List<MultiLabel> candidates = new ArrayList<>(candidateSet);
+
+        double[] probabilities = new double[candidates.size()];
+        if (config.getString("B").equals("1")){
+            for (int i=0;i<candidates.size();i++){
+                probabilities[i] = proba(candidates.get(i),uncalibratedLabelProbs);
+            }
+        } else {
+            for (int i=0;i<candidates.size();i++){
+                probabilities[i] = proba(candidates.get(i),calibratedLabelProbs);
+            }
+        }
+
+        if (config.getString("C").equals("1")){
+            double sum = MathUtil.arraySum(probabilities);
+            for (int i=0;i<probabilities.length;i++){
+                probabilities[i] /= sum;
+            }
+        }
+
+        Stream<Pair<Vector,Integer>> stream = IntStream.range(0,candidateSet.size())
+                .mapToObj(i->{
+                    Vector vector = new DenseVector(4);
+                    vector.set(0, probabilities[i]);
+                    vector.set(1,candidates.get(i).getNumMatchedLabels());
+                    Pair<Vector,Integer> pair = new Pair<>();
+                    pair.setFirst(vector);
+                    pair.setSecond(0);
+                    if (candidates.get(i).equals(dataSet.getMultiLabels()[data])){
+                        pair.setSecond(1);
+                    }
+                    return pair;
+                });
+
+        return stream;
+
+    }
+
+    private static double proba(MultiLabel multiLabel, double[] marginals){
+        double product = 1;
+        for (int l=0;l<marginals.length;l++){
+
+            if (multiLabel.matchClass(l)){
+                product *= marginals[l];
+            } else {
+                product *= 1-marginals[l];
+            }
+        }
+        return product;
     }
 }
