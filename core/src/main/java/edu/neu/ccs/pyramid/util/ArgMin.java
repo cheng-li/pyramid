fb92f1981318753d4df378212bf7fd08d75f1dc1
Cheng Li <chengli.email@gmail.com>
2018-08-22 15:17:21 -0400

    argmin

--- core/src/main/java/edu/neu/ccs/pyramid/util/ArgMin.java
+++ core/src/main/java/edu/neu/ccs/pyramid/util/ArgMin.java
@@ -1,5 +1,7 @@
 package edu.neu.ccs.pyramid.util;
 
+import java.util.List;
+
 /**
  * Created by chengli on 9/29/16.
  */
@@ -10,6 +12,20 @@ public class ArgMin {
         double min = Double.POSITIVE_INFINITY;
         for (int i=0;i<array.length;i++){
             double v = array[i];
+            if (v< min){
+                min = v;
+                res = i;
+            }
+        }
+        return res;
+    }
+
+
+    public static int argMin(List<Double> list){
+        int res = 0;
+        double min = Double.POSITIVE_INFINITY;
+        for (int i=0;i<list.size();i++){
+            double v = list.get(i);
             if (v< min){
                 min = v;
                 res = i;
