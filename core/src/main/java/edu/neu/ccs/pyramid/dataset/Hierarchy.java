0f3c5bdb05374285561406780365e122a13f4623
Cheng Li <chengli.email@gmail.com>
2018-12-22 12:25:57 -0500

    hierarchical arff parser

---
+++ core/src/main/java/edu/neu/ccs/pyramid/dataset/Hierarchy.java
@@ -0,0 +1,39 @@
+package edu.neu.ccs.pyramid.dataset;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hierarchy implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int numLabels;
+
+    public Hierarchy(int numLabels) {
+        this.numLabels = numLabels;
+        this.parents = new ArrayList<>(numLabels);
+        for (int l=0;l<numLabels;l++){
+            parents.add(new ArrayList<>());
+        }
+    }
+
+    private List<List<Integer>> parents;
+
+    public List<Integer> getParentsForLabel(int label){
+        return parents.get(label);
+    }
+
+    public void setParentsForLabel(int l, List<Integer> parents){
+        this.parents.get(l).clear();
+        this.parents.get(l).addAll(parents);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Hierarchy\n");
+        for (int l=0;l<numLabels;l++){
+            sb.append(l).append(": ").append(getParentsForLabel(l)).append("\n");
+        }
+        return sb.toString();
+    }
+}
