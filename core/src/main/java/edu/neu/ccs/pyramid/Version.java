86dda1db83d1255cbd0b16ba72cfaea880088e68
Cheng Li <chengli.email@gmail.com>
2019-03-21 22:22:00 -0400

    increment version to 0.11.1

--- core/src/main/java/edu/neu/ccs/pyramid/Version.java
+++ core/src/main/java/edu/neu/ccs/pyramid/Version.java
@@ -4,7 +4,7 @@ package edu.neu.ccs.pyramid;
  * Created by chengli on 9/9/15.
  */
 public class Version {
-    private static final String version = "0.11.0";
+    private static final String version = "0.11.1";
 
     public static String getVersion() {
         return version;
